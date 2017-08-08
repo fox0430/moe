3062b26ea782d7d2e64f8e886ea6e0c9adc82c09
fox0430 <ipoduser0430@gmail.com>
2017-08-08 00:58:39 +0000

    main

--- myEdiotr.c
+++ myEdiotr.c
@@ -2,23 +2,29 @@
 #include<stdio.h>
 #include<string.h>
 
+const int LineMax=999;
+
 int main(int argc,char *argv[]){
 
     FILE *fp;
     char fname[strlen(argv[1])+1];
     strcpy(fname,argv[1]);
-    int c, h, w, key;
+    int c, h, w, i, key;
     int x, y = 0;
+    char readline[LineMax][256]; 
     
-
-	initscr();      // start terminal contorl 
+    ESCDELAY = 25;      // delete esc time lag
+    
+    initscr();      // start terminal contorl 
     curs_set(1);    //set cursr
     keypad(stdscr, TRUE);   //enable cursr keys
 
 	getmaxyx(stdscr, h, w);     // set window size
 
 	start_color();      // color settings 
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);     // set color. 1 is white and black
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);     // set color. 1 is white and black
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);     // set color. 2 is green and black
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
 	bkgd(COLOR_PAIR(1));    // set default color
 
 
@@ -31,11 +37,15 @@ int main(int argc,char *argv[]){
 	erase();	// screen display 
 	move(0, 0);     // set default cursr point
 
-    while( (c = fgetc( fp )) != EOF ){      // display file contents
-        printw( "%c", c );
-	    refresh();
+    for(i=0; i<LineMax && fgets( readline[i] , sizeof(readline[i]) , fp ) != NULL;i++){
+        bkgd(COLOR_PAIR(2));    // color set
+        printw("%d: ", i);
+        bkgd(COLOR_PAIR(1));    // color set
+        printw( "%s", readline[i] );
+        refresh();
     }
 
+    move(0, 0);
 
     while (1) {     // input keys
 
@@ -43,17 +53,22 @@ int main(int argc,char *argv[]){
 		refresh();
 
 		key = getch();
-		if (key == 'q') break;
+
+		if (key == 27) break;
+        if (key == KEY_BACKSPACE) {
+            delch();
+            move(x--, y);
+        }
 
         switch (key) {
-		case KEY_UP:	y--; break;
-		case KEY_DOWN:	y++; break;
-		case KEY_LEFT:	x--; break;
-		case KEY_RIGHT:	x++; break;
-        default: echo(); x++;
+		case KEY_UP: y--; break;
+		case KEY_DOWN: y++; break;
+		case KEY_LEFT: x--; break;
+		case KEY_RIGHT: x++; break;
 		}
+
 	}
 
-	endwin();	// exit contorl 
+	endwin();	// exit control 
 	return (0);
 }
