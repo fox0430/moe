ecc644ee624370d0479a4a0a050b8fbebdc4d2bb
fox0430 <ipoduser0430@gmail.com>
2018-03-13 21:56:54 +0900

    improve printDirEntry()

--- fileManager.c
+++ fileManager.c
@@ -1,8 +1,6 @@
 #include<dirent.h>
 #include<ncurses.h>
 #include<stdlib.h>
-#include<string.h>
-#include<sys/types.h>
 #include"moe.h"
 
 int printDirEntry(WINDOW *win, struct dirent **nameList, int num);
@@ -10,7 +8,7 @@ int printDirEntry(WINDOW *win, struct dirent **nameList, int num);
 int fileManageMode(WINDOW **win, char *path){
   struct dirent **nameList;
 
-  int num = scandir(path, &nameList, NULL, NULL);
+  int num = scandir(path, &nameList, NULL, alphasort);
   if(num == -1){
     wprintw(win[2], "not found...");
     return -1;
