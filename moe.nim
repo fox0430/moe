91101fd19a74f3ee29f9c5152051db2d5bb2bedf
fox0430 <ipoduser0430@gmail.com>
2018-06-06 00:58:21 +0000

    Add editorStatus

--- moe.nim
+++ moe.nim
@@ -3,6 +3,18 @@ import posix
 import system
 import os
 
+type editorStatus = object
+  filename:               string
+  currentDir:             string
+  currentLine:            int 
+  positionInCurrentLine:  int
+  expandePosition:        int
+  mode:                   int
+  cmdLoop:                int
+  numOfChange:            int
+  debugMode:              int
+
+  
 proc startCurses() =
   discard setLocale(LC_ALL, "")
   initscr()
