16c8dde196051e0255addc42bfdf464ef26cbdc7
fox0430 <ipoduser0430@gmail.com>
2018-06-07 11:05:29 +0000

    Add editorStatus, main

--- src/moe.nim
+++ src/moe.nim
@@ -1,2 +1,22 @@
-# Hello Nim!
-echo "Hello, World!"
+import ncurses
+import posix
+import os
+import system
+
+type editorStatus = object
+  filename:               string
+  currentDir:             string
+  positionInCurrentLine:  int
+  expandePosition:        int
+  mode:                   int
+  cmdLoop:                int
+  numOfChange:            int
+  debugMode:              int
+
+if isMainModule:
+  var status = editorStatus()
+  if paramCount() == 0:
+    quit()
+  else:
+    status.filename = (string)os.commandLineParams()[0]
+    quit()
