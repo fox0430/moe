452f50a3970d46322f76444f899cfa094ca56ab9
fox0430 <ipoduser0430@gmail.com>
2018-06-07 14:22:22 +0000

    Add color setting

--- src/moe.nim
+++ src/moe.nim
@@ -3,6 +3,12 @@ import posix
 import os
 import system
 
+const COLOR_DEFAULT: int16 = -1   # default terminal color
+const BRIGHT_WHITE: int16 = 231
+const BRIGHT_GREEN: int16 = 85
+const GRAY: int16 = 245
+const LIGHT_BLUE: int16 = 14
+
 type EditorStatus = object
   filename:               string
   currentDir:             string
@@ -14,8 +20,30 @@ type EditorStatus = object
   numOfChange:            int
   debugMode:              int
 
+proc setCursesColor() =
+  start_color()   # enable color
+  use_default_colors()    # set terminal default color
+
+  init_pair(1, COLOR_BLACK , COLOR_GREEN)   # char is black, bg is green
+  init_pair(2, COLOR_BLACK, BRIGHT_WHITE)
+  init_pair(3, GRAY, COLOR_DEFAULT)
+  init_pair(4, COLOR_RED, COLOR_DEFAULT)
+  init_pair(5, COLOR_GREEN, COLOR_BLACK)
+  init_pair(6, BRIGHT_WHITE, COLOR_DEFAULT)
+  init_pair(7, BRIGHT_GREEN, COLOR_DEFAULT)
+  init_pair(8, LIGHT_BLUE, COLOR_DEFAULT)
+
 proc startCurses() =
-  initscr()
+  discard setLocale(LC_ALL, "")   # enable UTF-8
+  initscr()   # start terminal control
+  cbreak()    # enable cbreak mode
+  curs_set(1) # set cursor
+
+  var color_check: bool = can_change_color()
+  if color_check != true:
+    setCursesColor()
+
+  erase()
 
 proc exitCurses() =
   endwin()
