34d843941908d0496b6270529f05f23ff03da1b9
walkre <boo880432@gmail.com>
2018-06-20 01:33:02 +0900

    コマンド取得部分を抽出した

--- src/moepkg/exmode.nim
+++ src/moepkg/exmode.nim
@@ -1,25 +1,28 @@
 import strutils
 import editorstatus, ui
 
+proc getCommand(commandWindow: var Window): seq[string] =
+ var command = ""
+ while true:
+   commandWindow.erase
+   commandWindow.write(0, 0, ":"&command)
+   commandWindow.refresh
+
+   let key = commandWindow.getkey
+
+   if isEnterKey(key): break
+   if isBackspaceKey(key):
+     if command.len > 0: command.delete(command.high, command.high)
+     continue
+   if not key in 0..255: continue
+
+   command &= chr(key)
+
+ return command.splitWhitespace
+
 proc exMode*(status: var EditorStatus) =
-  var command = ""
-  while true:
-    status.commandWindow.erase
-    status.commandWindow.write(0, 0, ":"&command)
-    status.commandWindow.refresh
-
-    let key = status.commandWindow.getkey
-
-    if isEnterKey(key): break
-    if isBackspaceKey(key):
-      if command.len > 0: command.delete(command.high, command.high)
-      continue
-    if not key in 0..255: continue
-
-    command &= chr(key)
-
-  let args = command.splitWhitespace
-  if args.len == 1 and args[0] == "q":
+  let command = getCommand(status.commandWindow)
+  if command.len == 1 and command[0] == "q":
     status.mode = Mode.quit
   else:
     status.mode = Mode.normal
