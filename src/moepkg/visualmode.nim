9a02f5a256d609e84225ad2df4304d8854a67682
fox0430 <ipoduser0430@gmail.com>
2019-03-15 22:28:21 +0900

    Support select area in visual mode

--- src/moepkg/visualmode.nim
+++ src/moepkg/visualmode.nim
@@ -1,6 +1,12 @@
 import terminal
 import editorstatus, ui, normalmode, highlight, unicodeext
 
+type SelectArea = object
+  startLine: int
+  startColumn: int
+  endLine: int
+  endColumn: int
+
 proc initColorSegment(startLine, startColumn: int): ColorSegment =
   result.firstRow = startLine
   result.firstColumn = startColumn
@@ -8,17 +14,49 @@ proc initColorSegment(startLine, startColumn: int): ColorSegment =
   result.lastColumn = startColumn
   result.color = defaultMagenta
 
-proc updateColorSegment(colorSegment: var ColorSegment, lastLine, lastColumn: int) =
-  colorSegment.lastRow = lastLine
-  colorSegment.lastColumn = lastColumn
+proc initSelectArea(startLine, startColumn: int): SelectArea =
+  result.startLine = startLine
+  result.startColumn = startColumn
+  result.endLine = startLine
+  result.endColumn = startColumn
+
+proc updateSelectArea(area: var SelectArea, currentLine, currentColumn: int) =
+  area.endLine = currentLine
+  area.endColumn = currentColumn
+
+proc updateColorSegment(colorSegment: var ColorSegment, area: SelectArea) =
+  if area.startLine == area.endLine:
+    colorSegment.firstRow = area.startLine
+    colorSegment.lastRow = area.endLine
+    if area.startColumn < area.endColumn:
+      colorSegment.firstColumn = area.startColumn
+      colorSegment.lastColumn = area.endColumn
+    else:
+      colorSegment.firstColumn = area.endColumn
+      colorSegment.lastColumn = area.startColumn
+  elif area.startLine < area.endLine:
+    colorSegment.firstRow = area.startLine
+    colorSegment.lastRow = area.endLine
+    colorSegment.firstColumn = area.startColumn
+    colorSegment.lastColumn = area.endColumn
+  else:
+    colorSegment.firstRow = area.endLine
+    colorSegment.lastRow = area.startLine
+    colorSegment.firstColumn = area.endColumn
+    colorSegment.lastColumn = area.startColumn
 
 proc visualMode*(status: var EditorStatus) =
   status.resize(terminalHeight(), terminalWidth())
 
   var colorSegment = initColorSegment(status.currentLine, status.currentColumn)
+  var area = initSelectArea(status.currentLine, status.currentColumn)
 
   while status.mode == Mode.visual:
-    updateColorSegment(colorSegment, status.currentLine, status.currentColumn)
+
+    area.updateSelectArea(status.currentLine, status.currentColumn)
+    colorSegment.updateColorSegment(area)
+
+    status.updatehighlight
     status.highlight = status.highlight.overwrite(colorSegment)
     status.update
 
@@ -27,6 +65,7 @@ proc visualMode*(status: var EditorStatus) =
     if isResizekey(key):
       status.resize(terminalHeight(), terminalWidth())
     elif isEscKey(key):
+      status.updatehighlight
       status.changeMode(Mode.normal)
 
     elif key == ord('h') or isLeftKey(key) or isBackspaceKey(key):
