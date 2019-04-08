d5e0d47e5554902787a6fbf3463fbd7b85fcb391
fox0430 <ipoduser0430@gmail.com>
2019-04-08 19:16:19 +0900

    Fix #210

--- src/moepkg/cmdoption.nim
+++ src/moepkg/cmdoption.nim
@@ -15,7 +15,7 @@ proc parseCommandLineOption*(line: seq[string]): ComdParsedList  =
       of cmdShortOption, cmdLongOption:
         case key:
           of "v", "version":
-            echo "v0.0.4"
+            echo "v0.0.6"
             quit()
       of cmdEnd:
         assert(false)
