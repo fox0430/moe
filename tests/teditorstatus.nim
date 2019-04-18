d8e44d9d9202237d4bf100ec11d8271cc06c0ca1
fox0430 <ipoduser0430@gmail.com>
2019-04-18 13:49:57 +0900

    Fix editor status test

--- tests/teditorstatus.nim
+++ tests/teditorstatus.nim
@@ -5,14 +5,14 @@ test "resize 1":
   var status = initEditorStatus()
   status.buffer = initGapBuffer(@[ru"a"])
   status.highlight = initHighlight($status.buffer, status.language, status.settings.editorColor.editor)
-  status.view = initEditorView(status.buffer, 1, 1)
+  status.view.add(initEditorView(status.buffer, 1, 1))
   status.resize(0, 0)
 
 test "resize 2":
   var status = initEditorStatus()
   status.buffer = initGapBuffer(@[ru"a"])
   status.highlight = initHighlight($status.buffer, status.language, status.settings.editorColor.editor)
-  status.view = initEditorView(status.buffer, 20, 4)
+  status.view.add(initEditorView(status.buffer, 20, 4))
   status.resize(20, 4)
   status.currentColumn = 1
   status.changeMode(Mode.insert)
