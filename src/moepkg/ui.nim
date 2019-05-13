63ab46e66a3d90cf6b31f9265f1e582ee20307ad
fox0430 <ipoduser0430@gmail.com>
2019-05-13 10:46:00 +0900

    Add and fix color name

--- src/moepkg/ui.nim
+++ src/moepkg/ui.nim
@@ -21,20 +21,263 @@ type CursorType* = enum
   ibeamMode = 1
 
 type Color* = enum
-  default     = -1,
-  black       = 0,
-  red         = 1,
-  green       = 2,
-  yellow      = 3,
-  blue        = 4,
-  magenta     = 5,
-  cyan        = 6,
-  white       = 7,
-  lightBlue   = 14,
-  brightGreen = 85,
-  pink        = 198,
-  brightWhite = 231,
-  gray        = 245,
+  default             = -1,
+  black               = 0,
+  maroon              = 1,
+  green               = 2,
+  olive               = 3,
+  navy                = 4,
+  purple_1            = 5,
+  teal                = 6,
+  silver              = 7,
+  gray                = 8,
+  red                 = 9,
+  lime                = 10,
+  yellow              = 11,
+  blue                = 12,
+  fuchsia             = 13,
+  aqua                = 14,
+  white               = 15,
+  gray0               = 16,
+  navyBlue            = 17,
+  darkBlue            = 18,
+  blue3_1             = 19,
+  blue3_2             = 20,
+  blue1               = 21,
+  darkGreen           = 22,
+  deepSkyBlue4_1      = 23,
+  deepSkyBlue4_2      = 24,
+  deepSkyBlue4_3      = 25,
+  dodgerBlue3_1       = 26,
+  dodgerBlue3_2       = 27,
+  green4              = 28,
+  springGreen4        = 29,
+  turquoise4          = 30,
+  deepSkyBlue3_1      = 31,
+  deepSkyBlue3_2      = 32,
+  dodgerBlue1         = 33,
+  green3_1            = 34,
+  springGreen3_1      = 35,
+  darkCyan            = 36,
+  lightSeaGreen       = 37,
+  deepSkyBlue2        = 38,
+  deepSkyBlue1        = 39,
+  green3_2            = 40,
+  springGreen3_3      = 41,
+  springGreen2_1      = 42,
+  cyan3               = 43,
+  darkTurquoise       = 44,
+  turquoise2          = 45,
+  green1              = 46,
+  springGreen2_2      = 47,
+  springGreen1        = 48,
+  mediumSpringGreen   = 49,
+  cyan2               = 50,
+  cyan1               = 51,
+  darkRed_1           = 52,
+  deepPink4_1         = 53,
+  purple4_1           = 54,
+  purple4_2           = 55,
+  purple3             = 56,
+  blueViolet          = 57,
+  orange4_1           = 58,
+  gray37              = 59,
+  mediumPurple4       = 60,
+  slateBlue3_1        = 61,
+  slateBlue3_2        = 62,
+  royalBlue1          = 63,
+  chartreuse4         = 64,
+  darkSeaGreen4_1     = 65,
+  paleTurquoise4      = 66,
+  steelBlue           = 67,
+  steelBlue3          = 68,
+  cornflowerBlue      = 69,
+  chartreuse3_1       = 70,
+  darkSeaGreen4_2     = 71,
+  cadetBlue_1         = 72,
+  cadetBlue_2         = 73,
+  skyBlue3            = 74,
+  steelBlue1_1        = 75,
+  chartreuse3_2       = 76,
+  paleGreen3_1        = 77,
+  seaGreen3           = 78,
+  aquamarine3         = 79,
+  mediumTurquoise     = 80,
+  steelBlue1_2        = 81,
+  chartreuse2_1       = 82,
+  seaGreen2           = 83,
+  seaGreen1_1         = 84,
+  seaGreen1_2         = 85,
+  aquamarine1_1       = 86,
+  darkSlateGray2      = 87,
+  darkRed_2           = 88,
+  deepPink4_2         = 89,
+  darkMagenta_1       = 90,
+  darkMagenta_2       = 91,
+  darkViolet_1        = 92,
+  purple_2            = 93,
+  orange4_2           = 94,
+  lightPink4          = 95,
+  plum4               = 96,
+  mediumPurple3_1     = 97,
+  mediumPurple3_2     = 98,
+  slateBlue1          = 99,
+  yellow4_1           = 100,
+  wheat4              = 101,
+  gray53              = 102,
+  lightSlategray      = 103,
+  mediumPurple        = 104,
+  lightSlateBlue      = 105,
+  yellow4_2           = 106,
+  Wheat4              = 107,
+  darkSeaGreen        = 108,
+  lightSkyBlue3_1     =  109
+  lightSkyBlue3_2     =  110
+  skyBlue2            =  111
+  chartreuse2_2       =  112
+  darkOliveGreen3_1   =  113
+  paleGreen3_2        =  114
+  darkSeaGreen3_1     =  115
+  darkSlateGray3      =  116
+  skyBlue1            =  117
+  chartreuse1         =  118
+  lightGreen_1        =  119
+  lightGreen_2        =  120
+  paleGreen1_1        =  121
+  aquamarine1_2       =  122
+  darkSlateGray1      =  123
+  red3_1              =  124
+  deepPink4           =  125
+  mediumVioletRed     =  126
+  magenta3            =  127
+  darkViolet_2        =  128
+  purple              =  129
+  darkOrange3_1       =  130
+  indianRed_1         =  131
+  hotPink3_1          =  132
+  mediumOrchid3       =  133
+  mediumOrchid        =  134
+  mediumPurple2_1     =  135
+  darkGoldenrod       =  136
+  lightSalmon3_1      =  137
+  rosyBrown           =  138
+  gray63              =  139
+  mediumPurple2_2     =  140
+  mediumPurple1       =  141
+  gold3_1             =  142
+  darkKhaki           =  143
+  navajoWhite3        =  144
+  gray69              =  145
+  lightSteelBlue3     =  146
+  lightSteelBlue      =  147
+  yellow3_1           =  148
+  darkOliveGreen3_2   =  149
+  darkSeaGreen3_2     =  150
+  darkSeaGreen2_1     =  151
+  lightCyan3          =  152
+  lightSkyBlue1       =  153
+  greenYellow         =  154
+  darkOliveGreen2     =  155
+  paleGreen1_2        =  156
+  darkSeaGreen2_2     =  157
+  darkSeaGreen1_1     =  158
+  paleTurquoise1      =  159
+  red3_2              =  160
+  deepPink3_1         =  161
+  deepPink3_2         =  162
+  magenta3_1          =  163
+  magenta3_2          =  164
+  magenta2_1          =  165
+  darkOrange3_2       =  166
+  indianRed_2         =  167
+  hotPink3_2          =  168
+  hotPink2            =  169
+  orchid              =  170
+  mediumOrchid1_1     =  171
+  orange3             =  172
+  lightSalmon3_2      =  173
+  lightPink3          =  174
+  pink3               =  175
+  plum3               =  176
+  violet              =  177
+  gold3_2             =  178
+  lightGoldenrod3     =  179
+  tan                 =  180
+  mistyRose3          =  181
+  thistle3            =  182
+  plum2               =  183
+  yellow3_2           =  184
+  khaki3              =  185
+  lightGoldenrod2     =  186
+  lightYellow3        =  187
+  gray84              =  188
+  lightSteelBlue1     =  189
+  yellow2             =  190
+  darkOliveGreen1_1   =  191
+  darkOliveGreen1_2   =  192
+  darkSeaGreen1_2     =  193
+  honeydew2           =  194
+  lightCyan1          =  195
+  red1                =  196
+  deepPink2           =  197
+  deepPink1_1         =  198
+  deepPink1_2         =  199
+  magenta2_2          =  200
+  magenta1            =  201
+  orangeRed1          =  202
+  indianRed1_1        =  203
+  indianRed1_2        =  204
+  hotPink1_1           =  205
+  hotPink1_2           =  206
+  mediumOrchid1_2     =  207
+  darkOrange          =  208
+  salmon1             =  209
+  lightCoral          =  210
+  paleVioletRed1      =  211
+  orchid2             =  212
+  orchid1             =  213
+  orange1             =  214
+  sandyBrown          =  215
+  lightSalmon1        =  216
+  lightPink1          =  217
+  pink1               =  218
+  plum1               =  219
+  gold1               =  220
+  lightGoldenrod2_1   =  221
+  lightGoldenrod2_2   =  222
+  navajoWhite1        =  223
+  mistyRose1          =  224
+  thistle1            =  225
+  yellow1             =  226
+  lightGoldenrod1     =  227
+  khaki1              =  228
+  wheat1              =  229
+  cornsilk1           =  230
+  gray100             =  231
+  gray3               =  232
+  gray7               =  233
+  gray11              =  234
+  gray15              =  235
+  gray19              =  236
+  gray23              =  237
+  gray27              =  238
+  gray30              =  239
+  gray35              =  240
+  gray39              =  241
+  gray42              =  242
+  gray46              =  243
+  gray50              =  244
+  gray54              =  245
+  gray58              =  246
+  gray62              =  247
+  gray66              =  248
+  gray70              =  249
+  gray74              =  250
+  gray78              =  251
+  gray82              =  252
+  gray85              =  253
+  gray89              =  254
+  gray93              =  255
 
 type ColorPair* = enum
   blackGreen            = 1
@@ -135,24 +378,24 @@ proc setCursesColor() =
   use_default_colors()    # set terminal default color
 
   setColorPair(ColorPair.blackGreen, Color.black, Color.green)
-  setColorPair(ColorPair.blackWhite, Color.black, Color.brightWhite)
-  setColorPair(ColorPair.grayDefault, Color.gray, Color.default)
+  setColorPair(ColorPair.blackWhite, Color.black, Color.white)
+  setColorPair(ColorPair.grayDefault, Color.gray54, Color.default)
   setColorPair(ColorPair.redDefault, Color.red, Color.default)
   setColorPair(ColorPair.greenBlack, Color.green, Color.black)
-  setColorPair(ColorPair.brightWhiteDefault, Color.brightWhite, Color.default)
-  setColorPair(ColorPair.brightGreenDefault, Color.brightGreen, Color.default)
-  setColorPair(ColorPair.lightBlueDefault, Color.lightBlue, Color.default)
-  setColorPair(ColorPair.brightWhiteGreen, Color.brightWhite, Color.green)
-  setColorPair(ColorPair.cyanDefault, Color.cyan, Color.default)
-  setColorPair(ColorPair.whiteCyan, Color.white, Color.cyan)
-  setColorPair(ColorPair.magentaDefault, Color.magenta, Color.default)
+  setColorPair(ColorPair.brightWhiteDefault, Color.white, Color.default)
+  setColorPair(ColorPair.brightGreenDefault, Color.seaGreen1_2, Color.default)
+  setColorPair(ColorPair.lightBlueDefault, Color.aqua, Color.default)
+  setColorPair(ColorPair.brightWhiteGreen, Color.gray100, Color.green)
+  setColorPair(ColorPair.cyanDefault, Color.teal, Color.default)
+  setColorPair(ColorPair.whiteCyan, Color.white, Color.teal)
+  setColorPair(ColorPair.magentaDefault, Color.purple_1, Color.default)
   setColorPair(ColorPair.whiteDefault, Color.white, Color.default)
-  setColorPair(ColorPair.pinkDefault, Color.pink, Color.default)
-  setColorPair(ColorPair.blackPink, Color.black, Color.pink)
-  setColorPair(ColorPair.defaultMagenta, Color.default, Color.magenta)
+  setColorPair(ColorPair.pinkDefault, Color.deepPink1_1, Color.default)
+  setColorPair(ColorPair.blackPink, Color.black, Color.deepPink1_1)
+  setColorPair(ColorPair.defaultMagenta, Color.default, Color.purple_1)
   setColorPair(ColorPair.blackDefault, Color.black, Color.default)
-  setColorPair(ColorPair.cyanGray, Color.cyan, Color.gray)
-  setColorPair(ColorPair.brightWhiteBlue, Color.brightWhite, Color.blue)
+  setColorPair(ColorPair.cyanGray, Color.teal, Color.gray54)
+  setColorPair(ColorPair.brightWhiteBlue, Color.white, Color.blue)
   setColorPair(ColorPair.blueDefault, Color.blue, Color.default)
 
 proc setIbeamCursor*() =
@@ -196,8 +439,7 @@ proc startUi*() =
   keyEcho(false)
   set_escdelay(25)
 
-proc exitUi*() =
-  endwin()
+proc exitUi*() = endwin()
 
 proc initWindow*(height, width, top, left: int, color: ColorPair = ColorPair.brightWhiteDefault): Window =
   result.top = top
@@ -233,14 +475,11 @@ proc erase*(win: var Window) =
   win.y = 0
   win.x = 0
 
-proc refresh*(win: Window) =
-  wrefresh(win.cursesWindow)
+proc refresh*(win: Window) = wrefresh(win.cursesWindow)
 
-proc move*(win: Window, y, x: int) =
-  mvwin(win.cursesWindow, cint(y), cint(x))
+proc move*(win: Window, y, x: int) = mvwin(win.cursesWindow, cint(y), cint(x))
 
-proc resize*(win: Window, height, width: int) =
-  wresize(win.cursesWindow, cint(height), cint(width))
+proc resize*(win: Window, height, width: int) = wresize(win.cursesWindow, cint(height), cint(width))
 
 proc resize*(win: Window, height, width, y, x: int) =
   win.resize(height, width)
@@ -250,8 +489,7 @@ proc attron*(win: var Window, attributes: Attributes) = win.cursesWindow.wattron
 
 proc attroff*(win: var Window, attributes: Attributes) = win.cursesWindow.wattroff(cint(attributes))
 
-proc moveCursor*(win: Window, y, x: int) =
-  wmove(win.cursesWindow, cint(y), cint(x))
+proc moveCursor*(win: Window, y, x: int) = wmove(win.cursesWindow, cint(y), cint(x))
 
 let KEY_ESC = 27
 var KEY_RESIZE {.header: "<ncurses.h>", importc: "KEY_RESIZE".}: int
