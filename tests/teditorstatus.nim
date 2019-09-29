a96033d4625f0c821679dcc6a2967904924c9497
fox0430 <ipoduser0430@gmail.com>
2019-09-29 20:45:20 +0900

    Add tests in teditorstatus.nim

--- tests/teditorstatus.nim
+++ tests/teditorstatus.nim
@@ -1,5 +1,60 @@
 import terminal, strutils, unittest
-import moepkg/highlight, moepkg/editorstatus, moepkg/editorview, moepkg/gapbuffer, moepkg/unicodeext, moepkg/insertmode
+import moepkg/ui, moepkg/highlight, moepkg/editorstatus, moepkg/editorview, moepkg/gapbuffer, moepkg/unicodeext, moepkg/insertmode
+
+test "Add new buffer":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  status.addNewBuffer("")
+  check(status.bufStatus.len == 2)
+
+test "Split window":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  status.splitWindow
+
+test "Close window":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  status.splitWindow
+  status.closeWindow(1)
+  status.closeWindow(0)
+
+test "Move window 1":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  status.splitWindow
+  status.moveCurrentMainWindow(1)
+  check(status.currentMainWindow == 1)
+
+test "Move window 2":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  for i in 0 ..< 2: status.splitWindow
+  for i in 0 ..< 3: status.moveNextWindow
+  check(status.currentMainWindow == 2)
+
+  for i in 0 ..< 3: status.movePrevWindow
+  check(status.currentMainWindow == 0)
+
+test "Change theme":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+
+  status.settings.editorColorTheme = ColorTheme.dark
+  changeTheme(status)
+  status.resize(terminalHeight(), terminalWidth())
+
+  status.settings.editorColorTheme = ColorTheme.vivid
+  changeTheme(status)
+  status.resize(terminalHeight(), terminalWidth())
+
+  status.settings.editorColorTheme = ColorTheme.light
+  changeTheme(status)
+  status.resize(terminalHeight(), terminalWidth())
+
+  status.settings.editorColorTheme = ColorTheme.config
+  changeTheme(status)
+  status.resize(terminalHeight(), terminalWidth())
 
 test "resize 1":
   var status = initEditorStatus()
