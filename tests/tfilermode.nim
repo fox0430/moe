9d9e405af56f03e6bb2e9af174956d1940c3baef
fox0430 <ipoduser0430@gmail.com>
2020-02-07 02:38:17 +0900

    Fix #497

--- tests/tfilermode.nim
+++ tests/tfilermode.nim
@@ -1,9 +1,23 @@
 import unittest
-import moepkg/filermode, moepkg/editorstatus
+import moepkg/filermode, moepkg/editorstatus, moepkg/ui, moepkg/highlight
 
 test "Update directory list":
   var status = initEditorStatus()
   status.addNewBuffer("")
+  status.bufStatus[0].mode = Mode.filer
   var filerStatus = initFilerStatus()
   filerStatus = filerStatus.updateDirList
   status.updateFilerView(filerStatus)
+
+test "Check highlight in filer mode":
+  var status = initEditorStatus()
+  status.addNewBuffer("")
+  status.bufStatus[0].mode = Mode.filer
+  var filerStatus = initFilerStatus()
+  filerStatus = filerStatus.updateDirList
+  status.updateFilerView(filerStatus)
+
+  echo status.bufStatus[0].buffer
+  echo status.bufStatus[0].highlight
+
+  check(status.bufStatus[0].highlight[0].color == EditorColorPair.currentFile)
