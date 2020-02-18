9985eb34e3e0d05bd2a7463e05540a5e02a445ff
fox0430 <ipoduser0430@gmail.com>
2020-02-19 03:23:56 +0900

    Fix open buffer

--- src/moe.nim
+++ src/moe.nim
@@ -31,14 +31,16 @@ proc main() =
     quit()
   )
 
-  for p in parsedList:
-    if existsDir(p.filename):
-      try: setCurrentDir(p.filename)
-      except OSError:
-        status.commandWindow.writeFileOpenError(p.filename, status.messageLog)
-        addNewBuffer(status, "")
-      status.bufStatus.add(BufferStatus(mode: Mode.filer, lastSavetime: now()))
-    else: addNewBuffer(status, p.filename)
+  if parsedList.len > 0:
+    for p in parsedList:
+      if existsDir(p.filename):
+        try: setCurrentDir(p.filename)
+        except OSError:
+          status.commandWindow.writeFileOpenError(p.filename, status.messageLog)
+          addNewBuffer(status, "")
+        status.bufStatus.add(BufferStatus(mode: Mode.filer, lastSavetime: now()))
+      else: addNewBuffer(status, p.filename)
+  else: addNewBuffer(status, "")
 
   disableControlC()
 
