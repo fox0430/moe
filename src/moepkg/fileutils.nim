bff32b20cae84a6ed89eb7a081776b40933c34ae
fox0430 <ipoduser0430@gmail.com>
2020-06-01 02:11:09 +0900

    Refactor src/moepkg/fileutils.nim

--- src/moepkg/fileutils.nim
+++ src/moepkg/fileutils.nim
@@ -12,11 +12,14 @@ proc normalizePath*(path: seq[Rune]): seq[Rune] =
   else:
     return path
 
-proc openFile*(filename: seq[Rune]): tuple[text: seq[Rune], encoding: CharacterEncoding] =
+proc openFile*(filename: seq[Rune]): tuple[text: seq[Rune],
+                                           encoding: CharacterEncoding] =
+
   let
     raw = readFile($filename)
     encoding = detectCharacterEncoding(raw)
-    text =  if encoding == CharacterEncoding.unknown or encoding == CharacterEncoding.utf8:
+    text =  if encoding == CharacterEncoding.unknown or
+               encoding == CharacterEncoding.utf8:
       # 符号化形式が不明な場合は諦めてUTF-8としてUTF-32に変換する
       raw.toRunes
     else:
@@ -27,6 +30,12 @@ proc newFile*(): GapBuffer[seq[Rune]] =
   result = initGapBuffer[seq[Rune]]()
   result.add(ru"", false)
 
-proc saveFile*(filename: seq[Rune], runes: seq[Rune], encoding: CharacterEncoding) =
-  let buffer = convert($runes, $(if encoding == CharacterEncoding.unknown: CharacterEncoding.utf8 else: encoding),"UTF-8")
+proc saveFile*(filename: seq[Rune],
+               runes: seq[Rune],
+               encoding: CharacterEncoding) =
+
+  let
+    encode = if encoding == CharacterEncoding.unknown: CharacterEncoding.utf8
+             else: encoding
+    buffer = convert($runes, $(encode),"UTF-8")
   writeFile($filename, buffer)
