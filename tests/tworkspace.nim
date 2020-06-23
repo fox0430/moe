186ccb9fc4cf5d3eaf7a45d5f46448805e52b4a3
fox0430 <ipoduser0430@gmail.com>
2020-06-24 01:38:19 +0900

    FIx #728

--- tests/tworkspace.nim
+++ tests/tworkspace.nim
@@ -1,43 +1,56 @@
 import unittest
 import moepkg/[editorstatus]
 
-test "Work space: Create work space":
-  var status = initEditorStatus()
-  status.addNewBuffer("")
+suite "Work space":
+  test "Create work space":
+    var status = initEditorStatus()
+    status.addNewBuffer("")
+  
+    status.resize(100, 100)
+    status.update
+  
+    status.createWrokSpace
+  
+    check(status.workspace.len == 2)
 
-  status.resize(100, 100)
-  status.update
+  test "Create work space 2":
+    var status = initEditorStatus()
+    status.addNewBuffer("")
+  
+    status.resize(100, 100)
+    status.update
+  
+    status.createWrokSpace
+  
+    check status.workspace[0].currentMainWindowNode.bufferIndex == 0
+    check status.workspace[1].currentMainWindowNode.bufferIndex == 1
 
-  status.createWrokSpace
-
-  check(status.workspace.len == 2)
-
-test "Work space: Change current work space":
-  var status = initEditorStatus()
-  status.addNewBuffer("")
-
-  status.resize(100, 100)
-  status.update
-
-  status.createWrokSpace
-
-  ## Work space index is status.currentWorkSpaceIndex + 1
-  status.changeCurrentWorkSpace(1)
-  check(status.currentWorkSpaceIndex == 0)
-
-  status.changeCurrentWorkSpace(2)
-  check(status.currentWorkSpaceIndex == 1)
-
-test "Work space: Delete current work space":
-  var status = initEditorStatus()
-  status.addNewBuffer("")
-
-  status.resize(100, 100)
-  status.update
-
-  status.createWrokSpace
-
-  status.deleteWorkSpace(1)
-
-  check(status.workspace.len == 1)
-  check(status.currentWorkSpaceIndex == 0)
+  test "Change current work space":
+    var status = initEditorStatus()
+    status.addNewBuffer("")
+  
+    status.resize(100, 100)
+    status.update
+  
+    status.createWrokSpace
+  
+    ## Work space index is status.currentWorkSpaceIndex + 1
+    status.changeCurrentWorkSpace(1)
+    check(status.currentWorkSpaceIndex == 0)
+  
+    status.changeCurrentWorkSpace(2)
+    check(status.currentWorkSpaceIndex == 1)
+  
+  test "Delete current work space":
+    var status = initEditorStatus()
+    status.addNewBuffer("")
+  
+    status.resize(100, 100)
+    status.update
+  
+    status.createWrokSpace
+  
+    status.deleteWorkSpace(1)
+  
+    check(status.workspace.len == 1)
+    check(status.currentWorkSpaceIndex == 0)
