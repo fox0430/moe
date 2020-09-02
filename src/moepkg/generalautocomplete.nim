6eeca63868d086c581fecbb0c24568fe51d7f833
walkre <boo880432@gmail.com>
2020-09-02 16:29:27 +0900

    Add generalautocomplete.extractNeighborWord

--- src/moepkg/generalautocomplete.nim
+++ src/moepkg/generalautocomplete.nim
@@ -1,19 +1,37 @@
-import sugar
+import sugar, critbits
 import unicodedb, unicodedb/properties
 import unicodeext
 
+const
+  letterCharacter = ctgLu + ctgLl + ctgLt + ctgLm + ctgLo + ctgNl
+  combiningCharacter = ctgMn + ctgMc
+  decimalDigitCharacter = ctgNd
+  connectingCharacter = ctgPc
+  formattingCharacter = ctgCf
+const
+  firstCharacter = letterCharacter
+  succeedingCharacter = letterCharacter + combiningCharacter + decimalDigitCharacter + connectingCharacter + formattingCharacter + formattingCharacter
+
 iterator enumerateIdentifiers*(runes: seq[Rune]): seq[Rune] =
   # The name "enumerateIdentifiers" is a bit misleading because this proc also collects keywords in some programming language.
-  const
-    letterCharacter = ctgLu + ctgLl + ctgLt + ctgLm + ctgLo + ctgNl
-    combiningCharacter = ctgMn + ctgMc
-    decimalDigitCharacter = ctgNd
-    connectingCharacter = ctgPc
-    formattingCharacter = ctgCf
-  const
-    firstCharacter = letterCharacter
-    nonFirstCharacter = letterCharacter + combiningCharacter + decimalDigitCharacter + connectingCharacter + formattingCharacter + formattingCharacter
+  for identifier in split(runes, r => r.unicodeCategory notin succeedingCharacter, true):
+    if identifier[0].unicodeCategory notin firstCharacter: continue
+    yield identifier
 
-  for token in split(runes, r => r.unicodeCategory notin nonFirstCharacter, true):
-    if token[0].unicodeCategory notin firstCharacter: continue
-    yield token
+proc makeIdentifierDictionary*(runes: seq[Rune]): CritBitTree[void] =
+  for identifier in enumerateIdentifiers(runes):
+    result.incl($identifier)
+
+proc extractNeighborWord*(runes: seq[Rune], pos: int): seq[Rune] =
+  if runes[pos].unicodeCategory notin succeedingCharacter: return
+
+  var
+    first = pos
+    last = pos
+
+  while first-1 >= 0 and runes[first-1].unicodeCategory in succeedingCharacter:
+    dec(first)
+  while last+1 <= runes.high and runes[last+1].unicodeCategory in succeedingCharacter:
+    inc(last)
+
+  return runes[first..last]
--- tests/tgeneralautocomplete.nim
+++ tests/tgeneralautocomplete.nim
@@ -1,15 +1,38 @@
 import unittest, sugar, sequtils
-import moepkg/generalautocomplete, moepkg/unicodeext
+import moepkg/unicodeext
 
-test "enumerateIdentifiers":
-  const
-    code = ru"""proc fibonacci(n: int): int =
+include moepkg/generalautocomplete
+
+const code = ru"""proc fibonacci(n: int): int =
   if n == 0: return 0
   if n == 1: return 1
   return fibonacci(n - 1) + fibonacci(n - 2)"""
+ 
+test "enumerateIdentifiers":
+  const
     expectedResult = @["proc", "fibonacci", "n", "int", "int", "if", "n", "return", "if", "n", "return", "return", "fibonacci", "n", "fibonacci", "n"].map(ru)
     actualResult = collect(newSeq):
       for x in enumerateIdentifiers(code):
         x
 
   check actualResult == expectedResult
+
+test "makeIdentifierDictionary":
+  const allIdentifiers = @["proc", "fibonacci", "n", "int", "int", "if", "n", "return", "if", "n", "return", "return", "fibonacci", "n", "fibonacci", "n"].deduplicate
+  let dictionary = makeIdentifierDictionary(code)
+
+  for x in allIdentifiers:
+    check dictionary.contains(x)
+  for x in dictionary.items:
+    check allIdentifiers.contains(x)
+
+test "extractNeighborWord":
+  const text = ru"This is a sample text."
+
+  check extractNeighborWord(text, 0) == ru"This"
+  check extractNeighborWord(text, 3) == ru"This"
+  check extractNeighborWord(text, 4) == ru""
+  check extractNeighborWord(text, 7) == ru""
+  check extractNeighborWord(text, 8) == ru"a"
+  check extractNeighborWord(text, 20) == ru"text"
+  check extractNeighborWord(text, 21) == ru""
