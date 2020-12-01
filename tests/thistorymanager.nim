20f574b2bbc50d8c3149e60c49930aaf875b9667
fox0430 <ipoduser0430@gmail.com>
2020-12-01 21:16:48 +0900

    Add unit tests

--- tests/thistorymanager.nim
+++ tests/thistorymanager.nim
@@ -1,6 +1,6 @@
 import unittest, os
 import moepkg/[unicodetext, settings]
-include moepkg/historymanager
+include moepkg/[historymanager]
 
 suite "History Manager: Gnerate file name patern":
   test "Generate filename patern":
@@ -9,13 +9,28 @@ suite "History Manager: Gnerate file name patern":
 
 suite "History Manager: Get backup directory":
   test "Get backup directory in current dir":
+    const path = ru"test.nim"
     let
       settings = initEditorSettings()
-      dir = getBackupDir(ru"test.nim", settings.autoBackupSettings)
+      dir = getBackupDir(path, settings.autoBackupSettings)
     check dir == getCurrentDir().ru / ru".history"
 
   test "Get backup directory in other dir":
+    const path = ru"/tmp/test.nim"
     let
       settings = initEditorSettings()
-      dir = getBackupDir(ru"/tmp/test.nim", settings.autoBackupSettings)
+      dir = getBackupDir(path, settings.autoBackupSettings)
     check dir == ru"/tmp/.history"
+
+suite "History Manager: Generate backup file path":
+  test "From other directory":
+    const
+      originalFilePath = ru"src/moepkg/editorstatus.nim"
+      backupFileName =  ru"editorstatus_2020-12-01T17:34:22+09:00.nim"
+    let
+      settings = initEditorSettings()
+      p = generateBackUpFilePath(originalFilePath,
+                                 backupFileName,
+                                 settings.autoBackupSettings)
+
+    check p == ru"src/moepkg/.history/editorstatus_2020-12-01T17:34:22+09:00.nim"
