b2aa54d640c792a3642d24580c51cf588c9538de
fox0430 <ipoduser0430@gmail.com>
2020-12-08 12:15:27 +0900

    Fix #1211

--- src/moepkg/tabline.nim
+++ src/moepkg/tabline.nim
@@ -34,7 +34,7 @@ proc writeTabLineBuffer*(tabWin: var Window,
                 else: defaultColor
         currentMode = bufStatus.mode
         prevMode = bufStatus.prevMode
-        filename = if isFilerMode(currentMode, prevMode): getCurrentDir()
+        filename = if isFilerMode(currentMode, prevMode): $bufStatus.path
                    elif isHistoryManagerMode(currentMode, prevMode): "HISOTY"
                    elif isConfigMode(currentMode, prevMode): "CONFIG"
                    else: $bufStatus.path
@@ -51,7 +51,7 @@ proc writeTabLineBuffer*(tabWin: var Window,
         bufStatus = allBufStatus[bufIndex]
         currentMode = bufStatus.mode
         prevMode = bufStatus.prevMode
-        filename = if isFilerMode(currentMode, prevMode): getCurrentDir()
+        filename = if isFilerMode(currentMode, prevMode): $bufStatus.path
                    elif isHistoryManagerMode(currentMode, prevMode): "HISOTY"
                    elif isConfigMode(currentMode, prevMode): "CONFIG"
                    else: $bufStatus.path
