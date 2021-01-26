76ad74aeea38f42258a7c33cf35c246d951c1e81
fox0430 <ipoduser0430@gmail.com>
2021-01-27 02:56:34 +0900

    Exit Insert mode with Ctrl-C

--- src/moepkg/insertmode.nim
+++ src/moepkg/insertmode.nim
@@ -34,8 +34,20 @@ proc insertMode*(status: var EditorStatus) =
 
     var key = errorKey
     while key == errorKey:
-      status.eventLoopTask
-      key = getKey(currentMainWindowNode)
+      if not pressCtrlC:
+        status.eventLoopTask
+        key = getKey(currentMainWindowNode)
+      else:
+        # Exit insert mode
+
+        pressCtrlC = false
+
+        if currentMainWindowNode.currentColumn > 0:
+          dec(currentMainWindowNode.currentColumn)
+        currentMainWindowNode.expandedColumn = currentMainWindowNode.currentColumn
+        status.changeMode(Mode.normal)
+
+        return
 
     status.lastOperatingTime = now()
 
