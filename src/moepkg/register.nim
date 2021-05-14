638b73dfebe986140f65731f894afb8a8ae7d71d
fox0430 <ipoduser0430@gmail.com>
2021-05-14 09:44:34 +0900

    Fix moepkg/register.nim

--- src/moepkg/register.nim
+++ src/moepkg/register.nim
@@ -1,11 +1,11 @@
-import unicodeext, ui
+import unicodeext
 
 type Register* = object
   buffer*: seq[seq[Rune]]
   isLine*: bool
   name: string
 
-proc addRegister*(registers: var seq[Register],buffer: seq[Rune]) =
+proc addRegister*(registers: var seq[Register], buffer: seq[Rune]) =
   if registers.len == 0 or registers[^1].buffer != @[buffer]:
     registers.add Register(buffer: @[buffer], isLine: false)
 
@@ -21,6 +21,12 @@ proc addRegister*(registers: var seq[Register],
                   name: string) =
 
   if registers.len == 0 or registers[^1].buffer != @[buffer]:
+    # Overwrite the register if exist the same name.
+    for i, r in registers:
+      if r.name == name:
+        registers[i] = Register(buffer: @[buffer], isLine: false, name: name)
+        return
+
     registers.add Register(buffer: @[buffer], isLine: false, name: name)
 
 proc addRegister*(registers: var seq[Register],
@@ -29,6 +35,12 @@ proc addRegister*(registers: var seq[Register],
                   name: string) =
 
   if registers.len == 0 or registers[^1].buffer != @[buffer]:
+    # Overwrite the register if exist the same name.
+    for i, r in registers:
+      if r.name == name:
+        registers[i] = Register(buffer: @[buffer], isLine: isLine, name: name)
+        return
+
     registers.add Register(buffer: @[buffer], isLine: isLine, name: name)
 
 proc addRegister*(registers: var seq[Register], buffer: seq[seq[Rune]]) =
@@ -47,6 +59,12 @@ proc addRegister*(registers: var seq[Register],
                   name: string) =
 
   if registers.len == 0 or registers[^1].buffer != buffer:
+    # Overwrite the register if exist the same name.
+    for i, r in registers:
+      if r.name == name:
+        registers[i] = Register(buffer: buffer, isLine: true, name: name)
+        return
+
     registers.add Register(buffer: buffer, isLine: true, name: name)
 
 proc addRegister*(registers: var seq[Register],
@@ -55,7 +73,13 @@ proc addRegister*(registers: var seq[Register],
                   name: string) =
 
   if registers.len == 0 or registers[^1].buffer != buffer:
-    registers.add Register(buffer: buffer, isLine: isLine,  name: name)
+    # Overwrite the register if exist the same name.
+    for i, r in registers:
+      if r.name == name:
+        registers[i] = Register(buffer: buffer, isLine: isLine, name: name)
+        return
+
+    registers.add Register(buffer: buffer, isLine: isLine, name: name)
 
 proc searchByName(registers: seq[Register], name: string): Register =
   for r in registers:
