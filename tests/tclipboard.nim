0fcc64706846b6152498f7ea41d83b17c2a6b428
fox0430 <ipoduser0430@gmail.com>
2021-06-17 23:36:34 +0900

    Fix unit tests

--- tests/tclipboard.nim
+++ tests/tclipboard.nim
@@ -1,18 +1,176 @@
 import unittest
 import moepkg/[settings, unicodeext]
-include moepkg/[clipboard]
+include moepkg/[clipboard, platform]
 
-suite "Clipboard":
-  test "Send the text to the clipboard":
-    const buffer = @[ru "abc"]
-    sendToClipboard(buffer, settings.ClipboardToolOnLinux.xsel)
+proc initRegister(
+  buffer: seq[Rune],
+  settings: EditorSettings): register.Registers {.compiletime.} =
 
-    let r = execCmdEx("xsel -o")
-    check r.output == "abc\n"
+  result.addRegister(buffer, settings)
 
-  test "Send the text to the clipboard 2":
-    const buffer = @[ru "abc", ru "def"]
-    sendToClipboard(buffer, settings.ClipboardToolOnLinux.xsel)
+proc initRegister(
+  buffer: seq[seq[Rune]],
+  settings: EditorSettings): register.Registers {.compiletime.} =
 
-    let r = execCmdEx("xsel -o")
-    check r.output == "abc\ndef\n"
+  result.addRegister(buffer, settings)
+
+suite "Editor: Send to clipboad":
+  test "Send string to clipboard 1 (xsel)":
+    const
+      buffer = ru "Clipboard test"
+      tool = ClipboardToolOnLinux.xsel
+    let settings = initEditorSettings()
+
+    let registers = initRegister(buffer, settings)
+
+    let p = initPlatform()
+    if (p == Platforms.linux or
+        p == Platforms.wsl):
+      let
+        cmd = if p == Platforms.linux:
+                execCmdEx("xsel")
+              else:
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if p == Platforms.linux:
+        check output[0 .. output.high - 1] == $buffer
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $buffer
+
+  test "Send string to clipboard 1 (xclip)":
+    const
+      str = ru"Clipboard test"
+      registers = initRegister(str)
+      tool = ClipboardToolOnLinux.xclip
+
+    let platform = editorstatus.initPlatform()
+    registers.sendToClipboad(platform, tool)
+
+    if (platform == editorstatus.Platform.linux or
+        platform == editorstatus.Platform.wsl):
+      let
+        cmd = if platform == editorstatus.Platform.linux:
+                execCmdEx("xclip -o")
+              else:
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if platform == editorstatus.Platform.linux:
+        check output[0 .. output.high - 1] == $str
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $str
+
+  test "Send string to clipboard 2 (xsel)":
+    const
+      str = ru"`````"
+      registers = initRegister(str)
+      tool = ClipboardToolOnLinux.xsel
+
+    let platform = editorstatus.initPlatform()
+    registers.sendToClipboad(platform, tool)
+
+    if (platform == editorstatus.Platform.linux or
+        platform == editorstatus.Platform.wsl):
+      let
+        cmd = if platform == editorstatus.Platform.linux:
+                execCmdEx("xsel")
+              else:
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if platform == editorstatus.Platform.linux:
+        check output[0 .. output.high - 1] == $str
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $str
+
+  test "Send string to clipboard 2 (xclip)":
+    const
+      str = ru"`````"
+      registers = initRegister(str)
+      tool = ClipboardToolOnLinux.xclip
+
+    let platform = editorstatus.initPlatform()
+    registers.sendToClipboad(platform, tool)
+
+    if (platform == editorstatus.Platform.linux or
+        platform == editorstatus.Platform.wsl):
+      let
+        cmd = if platform == editorstatus.Platform.linux:
+                execCmdEx("xclip -o")
+              else:
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if platform == editorstatus.Platform.linux:
+        check output[0 .. output.high - 1] == $str
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $str
+
+  test "Send string to clipboard 3 (xsel)":
+    const
+      str = ru"$Clipboard test"
+      registers = initRegister(str)
+      tool = ClipboardToolOnLinux.xsel
+
+    let platform = editorstatus.initPlatform()
+    registers.sendToClipboad(platform, tool)
+
+    if (platform == editorstatus.Platform.linux or
+        platform == editorstatus.Platform.wsl):
+      let
+        cmd = if platform == editorstatus.Platform.linux:
+                execCmdEx("xsel")
+              else:
+
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if platform == editorstatus.Platform.linux:
+        check output[0 .. output.high - 1] == $str
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $str
+
+  test "Send string to clipboard 3 (xclip)":
+    const
+      str = ru"$Clipboard test"
+      registers = initRegister(str)
+      tool = ClipboardToolOnLinux.xclip
+
+    let platform = editorstatus.initPlatform()
+    registers.sendToClipboad(platform, tool)
+
+    if (platform == editorstatus.Platform.linux or
+        platform == editorstatus.Platform.wsl):
+      let
+        cmd = if platform == editorstatus.Platform.linux:
+                execCmdEx("xclip -o")
+              else:
+
+                # On the WSL
+                execCmdEx("powershell.exe -Command Get-Clipboard")
+        (output, exitCode) = cmd
+
+      check exitCode == 0
+      if platform == editorstatus.Platform.linux:
+        check output[0 .. output.high - 1] == $str
+      else:
+        # On the WSL
+        check output[0 .. output.high - 2] == $str
+
+
