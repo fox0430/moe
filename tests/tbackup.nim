f3659e28e0fa6e8e364663af8a524669d5057cbf
fox0430 <shuu.n@protonmail.com>
2022-10-28 07:52:43 +0900

    test: Rewite unit tests for the auto backups

--- tests/tbackup.nim
+++ tests/tbackup.nim
@@ -1,45 +1,177 @@
-import std/[unittest, times, os]
+import std/[unittest, times, os, json, oids, sequtils]
 import moepkg/unicodeext
 include moepkg/backup
 
-suite "Backup: Generate filename":
-  test "The same path that opened the editor":
+suite "Backup: createDir":
+  test "createDir":
+    let dirName = $now()
+
+    check createDir(dirName.toRunes)
+    check dirExists(dirName)
+
+    # Clean up
+    removeDir(dirName)
+
+  test "createDir 2":
+    let dirName = $now()
+
+    os.createDir(dirName)
+
+    let r = createDir(dirName.toRunes)
+
+    # Clean up
+    removeDir(dirName)
+
+    check r
+
+suite "Backup: validateBackupJson":
+  test "validateBackupJson":
+    let jsonNode = %* { "path" : "path" }
+    check validateBackupInfoJson(jsonNode)
+
+  test "validateBackupJson 2":
+    # Invalid json
+    let jsonNode = %* { "a" : "path" }
+    check not validateBackupInfoJson(jsonNode)
+
+  test "validateBackupJson 3":
+    # Invalid json
+    let jsonNode = %* { "path" : 0 }
+    check not validateBackupInfoJson(jsonNode)
+
+suite "Backup: validateBackupFileName":
+  test "validateBackupFileName":
+    let filename = "2022-10-26T08:28:50+09:00"
+    check validateBackupFileName(filename)
+
+  test "validateBackupFileName 2":
+    # Invalid filename
+    let filename = "2022-10-26T08:28:50"
+    check not validateBackupFileName(filename)
+
+suite "Backup: backupDir":
+  let
+    baseBackupDir = getCurrentDir() / "baseBackupDirForTest"
+    backupDir = baseBackupDir / $genOid()
+    sourceFilePath = getCurrentDir() / "source.txt"
+    backupInfoJsonNode = %* { "path": sourceFilePath }
+
+  setup:
+    os.createDir(baseBackupDir)
+
+    os.createDir(backupDir)
+
+    let backupInfoJsonPath = backupDir / "backup.json"
+    writeFile(backupInfoJsonPath, $backupInfoJsonNode)
+
+    for _ in 0 .. 4:
+      # Dummy files
+      let backupDir = baseBackupDir / $genOid()
+      os.createDir(backupDir)
+
+      let
+        backupJsonInfoPath = backupDir / "backup.json"
+        sourceFilePath = getCurrentDir() / "source-1.txt"
+        backupInfoJsonNode = %* { "path": sourceFilePath }
+      writeFile(backupJsonInfoPath, $backupInfoJsonNode)
+
+  teardown:
+    removeDir(baseBackupDir)
+
+  test "backupDir":
+    check backupDir(baseBackupDir, sourceFilePath) == backupDir
+
+suite "Backup: getBackupFiles":
+  let
+    baseBackupDir = getCurrentDir() / "baseBackupDirForTest"
+    backupDir = baseBackupDir / $genOid()
+    sourceFilePath = getCurrentDir() / "source.txt"
+    backupInfoJsonNode = %* { "path": sourceFilePath }
+
+  setup:
+    os.createDir(baseBackupDir)
+
+    os.createDir(backupDir)
+
+    let backupInfoJsonPath = backupDir / "backup.json"
+    writeFile(backupInfoJsonPath, $backupInfoJsonNode)
+
+    var backupFilenames: seq[seq[Rune]]
+    for _ in 0 .. 2:
+      backupFilenames.add now().toRunes
+      writeFile(backupDir / $backupFilenames[^1], "\n")
+
+    for _ in 0 .. 4:
+      # Dummy files
+      let backupDir = baseBackupDir / $genOid()
+      os.createDir(backupDir)
+
+      let
+        backupJsonInfoPath = backupDir / "backup.json"
+        sourceFilePath = getCurrentDir() / "source-1.txt"
+        backupInfoJsonNode = %* { "path": sourceFilePath }
+      writeFile(backupJsonInfoPath, $backupInfoJsonNode)
+
+  teardown:
+    removeDir(baseBackupDir)
+
+  test "getBackupFiles":
+    let r = getBackupFiles(baseBackupDir.toRunes, sourceFilePath.toRunes)
+    for f in backupFilenames:
+      check r.in f
+
+suite "Backup: initBackupDir":
+  let baseBackupDir = getCurrentDir() / "baseBackupDirForTest"
+
+  teardown:
+    removeDir(baseBackupDir)
+
+  test "initBackupDir":
     let
-      time = now()
-      filename = ru"test.nim"
-      backupFilename = generateFilename(filename, time)
+      sourceFilePath = getCurrentDir() / "source.txt"
+      backupDir = initBackupDir(baseBackupDir.toRunes, sourceFilePath.toRunes)
 
-    check backupFilename == ru"test_" & ($time).toRunes & ru".nim"
+    check dirExists($backupDir)
 
-  test "The path that opened the editor and a different path":
+suite "Backup: writeBackupFile":
+  let
+    baseBackupDir = getCurrentDir() / "baseBackupDirForTest"
+    backupDir = baseBackupDir / $genOid()
+
+  setup:
+    os.createDir(baseBackupDir)
+    os.createDir(backupDir)
+
+  teardown:
+    removeDir(baseBackupDir)
+
+  test "writeBackupFile":
+    const
+      BUFFER = ru"abc"
+      ENCODING = CharacterEncoding.utf8
     let
-      time = now()
-      filename = ru"/tmp/hoge/test.nim"
-      backupFilename = generateFilename(filename, time)
+      backupFilename = now().toRunes
+      backupFilePath = backupDir.toRunes / backupFilename
 
-    check backupFilename == ru"test_" & ($time).toRunes & ru".nim"
+    check writeBackupFile(backupFilePath, BUFFER, ENCODING)
+    check fileExists(backupDir / $backupFilename)
 
-  test "The path contains non-US-ASCII characters (issue #936)":
-    let
-      time = now()
-      filename = ru"/tmp/ディレクトリ/test.nim"
-      backupFilename = generateFilename(filename, time)
+suite "Backup: writeBackupInfoJson":
+  let
+    baseBackupDir = getCurrentDir() / "baseBackupDirForTest"
+    backupDir = baseBackupDir / $genOid()
 
-    check backupFilename == ru"test_" & ($time).toRunes & ru".nim"
+  setup:
+    os.createDir(baseBackupDir)
+    os.createDir(backupDir)
 
-suite "Backup: Generate backup dir":
-  test "The same path that opened the editor":
-    let
-      time = now()
-      filename = ru"test.nim"
-      dir = checkAndCreateBackupDir(filename, ru"")
+  teardown:
+    removeDir(baseBackupDir)
 
-    check dir == ru".history"
+  test "writeBackupInfoJson":
+    const SOURCE_FILE_PATH = "/source/file/path".toRunes
+    check writeBackupInfoJson(backupDir.toRunes, SOURCE_FILE_PATH)
 
-  test "The path that opened the editor and a different path":
-    let
-      time = now()
-      path = ru"/tmp/hoge/test.nim"
-      dir = checkAndCreateBackupDir(path, ru"")
-
-    check dir == ru"/tmp/hoge/.history"
+    let backupInfoJsonPath = $backupInfoJsonPath(backupDir.toRunes)
+    check fileExists(backupInfoJsonPath)
+    check json.parseFile(backupInfoJsonPath) == %* { "path": $SOURCE_FILE_PATH}
