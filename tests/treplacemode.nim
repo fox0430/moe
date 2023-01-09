649e99dd8b36b1c9f9347efcba7c012773b95060
fox0430 <shuu.n@protonmail.com>
2023-01-09 12:16:41 +0900

    test: Fix tests/treplacemode.nim

--- tests/treplacemode.nim
+++ tests/treplacemode.nim
@@ -1,177 +1,128 @@
 import std/unittest
-import moepkg/[unicodeext]
 include moepkg/replacemode
 
 template recordCurrentPosition() =
-  let
-    windowNode = currentMainWindowNode
-    bufferIndex = windowNode.bufferIndex
-
-  status.bufStatus[bufferIndex].buffer.beginNewSuitIfNeeded
-  status.bufStatus[bufferIndex].tryRecordCurrentPosition(windowNode)
+  currentBufStatus.buffer.beginNewSuitIfNeeded
+  currentBufStatus.tryRecordCurrentPosition(currentMainWindowNode)
 
 suite "Replace mode: Replace current Character":
   test "Replace current character":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
 
-    var
-      isMoved = false
-      undoLastSuitId =
-        status.bufStatus[0].buffer.lastSuitId
     const key = ru'z'
-
-    status.bufStatus[0].replaceCurrentCharacter(
+    currentBufStatus.replaceCurrentCharacter(
       currentMainWindowNode,
-      isMoved,
-      key,
-      status.settings)
+      status.settings,
+      key)
 
-    check status.bufStatus[0].buffer[0] == ru"zbc"
+    check currentBufStatus.buffer[0] == ru"zbc"
 
   test "Replace current character 2":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
-
-    var
-      isMoved = false
-      undoLastSuitId =
-        status.bufStatus[0].buffer.lastSuitId
-    const key = ru'z'
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
 
     for i in 0 ..< 5:
-      status.bufStatus[0].replaceCurrentCharacter(
+      const key = ru'z'
+      currentBufStatus.replaceCurrentCharacter(
         currentMainWindowNode,
-        isMoved,
-        key,
-        status.settings)
+        status.settings,
+        key)
 
-    check status.bufStatus[0].buffer[0] == ru"zzzzz"
+    check currentBufStatus.buffer[0] == ru"zzzzz"
 
 suite "Replace mode: Undo":
   test "undo":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
 
-    var
-      isMoved = false
-      undoLastSuitId =
-        status.bufStatus[0].buffer.lastSuitId
+    recordCurrentPosition()
+
     const key = ru'z'
+    currentBufStatus.replaceCurrentCharacter(
+      currentMainWindowNode,
+      status.settings,
+      key)
 
     recordCurrentPosition()
 
-    status.bufStatus[0].replaceCurrentCharacter(
-      currentMainWindowNode,
-      isMoved,
-      key,
-      status.settings)
+    currentBufStatus.undoOrMoveCursor(
+      currentMainWindowNode)
 
-    recordCurrentPosition()
-
-    status.bufStatus[0].undoOrMoveCursor(
-      currentMainWindowNode,
-      isMoved,
-      undoLastSuitId)
-
-    check status.bufStatus[0].buffer[0] == ru"abc"
+    check currentBufStatus.buffer[0] == ru"abc"
 
   test "undo 2":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
 
-    var
-      isMoved = false
-      undoLastSuitId =
-        status.bufStatus[0].buffer.lastSuitId
+    recordCurrentPosition()
+
     const key = ru'z'
+    currentBufStatus.replaceCurrentCharacter(
+      currentMainWindowNode,
+      status.settings,
+      key)
 
     recordCurrentPosition()
 
-    status.bufStatus[0].replaceCurrentCharacter(
-      currentMainWindowNode,
-      isMoved,
-      key,
-      status.settings)
+    currentBufStatus.moveRight(currentMainWindowNode)
 
-    recordCurrentPosition()
+    currentBufStatus.undoOrMoveCursor(currentMainWindowNode)
 
-    status.bufStatus[0].moveRight(currentMainWindowNode,
-                                  isMoved,
-                                  undoLastSuitId)
-
-    status.bufStatus[0].undoOrMoveCursor(
-      currentMainWindowNode,
-      isMoved,
-      undoLastSuitId)
-
-    check status.bufStatus[0].buffer[0] == ru"zbc"
+    check currentBufStatus.buffer[0] == ru"zbc"
 
   test "undo 3":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
     currentMainWindowNode.currentColumn = 2
 
-    var
-      isMoved = false
-      undoLastSuitId =
-        status.bufStatus[0].buffer.lastSuitId
+    recordCurrentPosition()
+
     const key = ru'z'
+    currentBufStatus.replaceCurrentCharacter(
+      currentMainWindowNode,
+      status.settings,
+      key)
 
     recordCurrentPosition()
 
-    status.bufStatus[0].replaceCurrentCharacter(
-      currentMainWindowNode,
-      isMoved,
-      key,
-      status.settings)
+    currentBufStatus.moveRight(currentMainWindowNode)
 
-    recordCurrentPosition()
+    currentBufStatus.undoOrMoveCursor(currentMainWindowNode)
 
-    status.bufStatus[0].moveRight(currentMainWindowNode,
-                                  isMoved,
-                                  undoLastSuitId)
-
-    status.bufStatus[0].undoOrMoveCursor(
-      currentMainWindowNode,
-      isMoved,
-      undoLastSuitId)
-
-    check status.bufStatus[0].buffer[0] == ru"abc"
+    check currentBufStatus.buffer[0] == ru"abc"
 
 suite "Replace mode: New line":
   test "New line and replace character":
     var status = initEditorStatus()
     status.addNewBuffer
-    status.bufStatus[0].mode = Mode.replace
-    status.bufStatus[0].buffer = initGapBuffer(@[ru"abc"])
+    currentBufStatus.mode = Mode.replace
+    currentBufStatus.buffer = initGapBuffer(@[ru"abc"])
     currentMainWindowNode.currentColumn = 1
 
     recordCurrentPosition()
 
-    status.bufStatus[0].keyEnter(currentMainWindowNode,
-                                 status.settings.autoIndent,
-                                 status.settings.tabStop)
-
-    var isMoved = false
-    const key = ru'z'
-
-    status.bufStatus[0].replaceCurrentCharacter(
+    currentBufStatus.keyEnter(
       currentMainWindowNode,
-      isMoved,
-      key,
-      status.settings)
+      status.settings.autoIndent,
+      status.settings.tabStop)
 
-    check status.bufStatus[0].buffer.len == 2
-    check status.bufStatus[0].buffer[0] == ru"a"
-    check status.bufStatus[0].buffer[1] == ru"zc"
+    const key = ru'z'
+    currentBufStatus.replaceCurrentCharacter(
+      currentMainWindowNode,
+      status.settings,
+      key)
+
+    check currentBufStatus.buffer.len == 2
+    check currentBufStatus.buffer[0] == ru"a"
+    check currentBufStatus.buffer[1] == ru"zc"
