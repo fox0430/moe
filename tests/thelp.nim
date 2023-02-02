fd525de95397bbcd46209a71d92a66a44d988ef9
fox0430 <shuu.n@protonmail.com>
2023-02-03 08:26:06 +0900

    Add tests

--- tests/thelp.nim
+++ tests/thelp.nim
@@ -1,9 +1,11 @@
-import std/[unittest, strutils]
-import moepkg/[unicodeext, gapbuffer]
+import std/[unittest, strutils, importutils]
+import moepkg/[unicodeext, gapbuffer, editorstatus, bufferstatus]
 
 import moepkg/helputils {.all.}
+import moepkg/help {.all.}
+import moepkg/commandline {.all.}
 
-suite "Help":
+suite "initHlepModeBuffer":
   test "initHlepModeBuffer":
     let
       buffer = initHelpModeBuffer().toGapBuffer
@@ -11,3 +13,20 @@ suite "Help":
 
     for i in 0 ..< buffer.len:
       check $buffer[i] == help[i]
+
+suite "execHelpCommand":
+  test "':' key":
+    var status = initEditorStatus()
+
+    const path = ""
+    status.addNewBufferInCurrentWin(path, Mode.help)
+    status.resize
+
+    const key = ":".toRunes
+    status.execHelpCommand(key)
+
+    check currentBufStatus.isExmode
+    check status.commandLine.buffer.len == 0
+
+    privateAccess(status.commandLine.type)
+    check status.commandLine.prompt == ":".toRunes
