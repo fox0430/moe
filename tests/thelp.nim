71b09fc170c0a7db82b6f22da80efb2ddfe9a457
Shuu <Shuu.N@protonmail.com>
2023-02-05 00:20:48 +0900

    Fix #1609 (#1610)
    * Fix #1609
    
    Fix a bug in that the prompt is not displayed when changing the mode from help
    to ex.
    
    * Add tests
    
    * Fix test

--- tests/thelp.nim
+++ tests/thelp.nim
@@ -1,13 +1,32 @@
-import std/[unittest, strutils]
-import moepkg/[unicodeext, gapbuffer]
+import std/[unittest, strutils, importutils]
+import moepkg/[unicodeext, gapbuffer, editorstatus, bufferstatus]
 
 import moepkg/helputils {.all.}
+import moepkg/help {.all.}
+import moepkg/commandline {.all.}
 
-suite "Help":
-  test "initHlepModeBuffer":
+suite "initHelpModeBuffer":
+  test "initHelpModeBuffer":
     let
       buffer = initHelpModeBuffer().toGapBuffer
       help = helpsentences.splitLines
 
     for i in 0 ..< buffer.len:
       check $buffer[i] == help[i]
+
+suite "execHelpCommand":
+  test "':' key":
+    var status = initEditorStatus()
+
+    const path = ""
+    status.addNewBufferInCurrentWin(path, Mode.help)
+    status.resize
+
+    const key = ":".toRunes
+    status.execHelpCommand(key)
+
+    check currentBufStatus.isExmode
+    check status.commandLine.buffer.len == 0
+
+    privateAccess(status.commandLine.type)
+    check status.commandLine.prompt == exModePrompt.toRunes
