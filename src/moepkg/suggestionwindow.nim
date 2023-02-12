44a467e4760670ac1988241b10ea44c05a078100
Shuu <Shuu.N@protonmail.com>
2023-02-13 03:49:49 +0900

    refactor: Fix indent in suggestionwindow.nim (#1622)

--- src/moepkg/suggestionwindow.nim
+++ src/moepkg/suggestionwindow.nim
@@ -68,47 +68,47 @@ proc handleKeyInSuggestionWindow*(
   windowNode: var WindowNode,
   key: Rune) =
 
-  doAssert(canHandleInSuggestionWindow(key))
+    doAssert(canHandleInSuggestionWindow(key))
 
-  # Check whether the selected suggestion is changed.
-  let prevSuggestion = suggestionWindow.selectedSuggestion
+    # Check whether the selected suggestion is changed.
+    let prevSuggestion = suggestionWindow.selectedSuggestion
 
-  if isTabKey(key) or isDownKey(key):
-    if suggestionWindow.selectedSuggestion == suggestionWindow.suggestoins.high:
-      suggestionWindow.selectedSuggestion = 0
-    else:
-      inc(suggestionWindow.selectedSuggestion)
-  elif isShiftTab(key) or isUpKey(key):
-    if suggestionWindow.selectedSuggestion == 0:
-      suggestionWindow.selectedSuggestion = suggestionWindow.suggestoins.high
-    else:
-      dec(suggestionWindow.selectedSuggestion)
-  elif isPageDownKey(key):
-    suggestionWindow.selectedSuggestion +=
-      suggestionWindow.popUpWindow.get.height - 1
-  elif isPageUpKey(key):
-    suggestionWindow.selectedSuggestion -=
-      suggestionWindow.popUpWindow.get.height - 1
+    if isTabKey(key) or isDownKey(key):
+      if suggestionWindow.selectedSuggestion == suggestionWindow.suggestoins.high:
+        suggestionWindow.selectedSuggestion = 0
+      else:
+        inc(suggestionWindow.selectedSuggestion)
+    elif isShiftTab(key) or isUpKey(key):
+      if suggestionWindow.selectedSuggestion == 0:
+        suggestionWindow.selectedSuggestion = suggestionWindow.suggestoins.high
+      else:
+        dec(suggestionWindow.selectedSuggestion)
+    elif isPageDownKey(key):
+      suggestionWindow.selectedSuggestion +=
+        suggestionWindow.popUpWindow.get.height - 1
+    elif isPageUpKey(key):
+      suggestionWindow.selectedSuggestion -=
+        suggestionWindow.popUpWindow.get.height - 1
 
-  suggestionWindow.selectedSuggestion =
-    suggestionWindow.selectedSuggestion.clamp(0, suggestionWindow.suggestoins.high)
+    suggestionWindow.selectedSuggestion =
+      suggestionWindow.selectedSuggestion.clamp(0, suggestionWindow.suggestoins.high)
 
-  if suggestionWindow.selectedSuggestion != prevSuggestion:
-    # The selected suggestoin is changed.
-    # Update the buffer without recording the change.
-    let newLine = suggestionWindow.newLine
-    bufStatus.buffer.assign(newLine, windowNode.currentLine, false)
+    if suggestionWindow.selectedSuggestion != prevSuggestion:
+      # The selected suggestoin is changed.
+      # Update the buffer without recording the change.
+      let newLine = suggestionWindow.newLine
+      bufStatus.buffer.assign(newLine, windowNode.currentLine, false)
 
-    let
-      firstColumn =
-        if (suggestionWindow.isPath) and (newLine in '/'.ru):
-          suggestionWindow.firstColumn + 1
-        else:
-          suggestionWindow.firstColumn
-      wordLen = suggestionWindow.selectedWordOrInputWord.len
-    windowNode.currentColumn = firstColumn + wordLen
+      let
+        firstColumn =
+          if (suggestionWindow.isPath) and (newLine in '/'.ru):
+            suggestionWindow.firstColumn + 1
+          else:
+            suggestionWindow.firstColumn
+        wordLen = suggestionWindow.selectedWordOrInputWord.len
+      windowNode.currentColumn = firstColumn + wordLen
 
-    bufStatus.isUpdate = true
+      bufStatus.isUpdate = true
 
 # Suggestions are extracted from `text`.
 # `word` is the inputted text.
@@ -119,55 +119,55 @@ proc initSuggestionWindow*(
   firstColumn, lastColumn: int,
   isPath: bool): Option[SuggestionWindow] =
 
-  if not isPath:
-    wordDictionary.addWordToDictionary(text)
+    if not isPath:
+      wordDictionary.addWordToDictionary(text)
 
-  var suggestionWindow: SuggestionWindow
-  suggestionWindow.wordDictionary = wordDictionary
-  suggestionWindow.inputWord = word
-  suggestionWindow.firstColumn = firstColumn
-  suggestionWindow.lastColumn = lastColumn
-  suggestionWindow.isPath = isPath
+    var suggestionWindow: SuggestionWindow
+    suggestionWindow.wordDictionary = wordDictionary
+    suggestionWindow.inputWord = word
+    suggestionWindow.firstColumn = firstColumn
+    suggestionWindow.lastColumn = lastColumn
+    suggestionWindow.isPath = isPath
 
-  if isPath:
-    suggestionWindow.suggestoins = text.splitWhitespace
-  else:
-    suggestionWindow.suggestoins = collectSuggestions(
-      suggestionWindow.wordDictionary,
-      word)
+    if isPath:
+      suggestionWindow.suggestoins = text.splitWhitespace
+    else:
+      suggestionWindow.suggestoins = collectSuggestions(
+        suggestionWindow.wordDictionary,
+        word)
 
-  if suggestionWindow.suggestoins.len == 0: return none(SuggestionWindow)
+    if suggestionWindow.suggestoins.len == 0: return none(SuggestionWindow)
 
-  suggestionWindow.selectedSuggestion = -1
-  suggestionWindow.oldLine = currentLineText
+    suggestionWindow.selectedSuggestion = -1
+    suggestionWindow.oldLine = currentLineText
 
-  return some(suggestionWindow)
+    return some(suggestionWindow)
 
 proc extractWordBeforeCursor(
   bufStatus: BufferStatus,
   windowNode: WindowNode): Option[tuple[word: seq[Rune], first, last: int]] =
 
-  if windowNode.currentColumn - 1 < 0: return
-  extractNeighborWord(
-    bufStatus.buffer[windowNode.currentLine],
-    windowNode.currentColumn - 1)
+    if windowNode.currentColumn - 1 < 0: return
+    extractNeighborWord(
+      bufStatus.buffer[windowNode.currentLine],
+      windowNode.currentColumn - 1)
 
 proc extractPathBeforeCursor(
   bufStatus: BufferStatus,
   windowNode: WindowNode): Option[tuple[path: seq[Rune], first, last: int]] =
 
-  if windowNode.currentColumn - 1 < 0: return
-  extractNeighborPath(
-    bufStatus.buffer[windowNode.currentLine],
-    windowNode.currentColumn - 1)
+    if windowNode.currentColumn - 1 < 0: return
+    extractNeighborPath(
+      bufStatus.buffer[windowNode.currentLine],
+      windowNode.currentColumn - 1)
 
 proc wordExistsBeforeCursor(
   bufStatus: BufferStatus,
   windowNode: WindowNode): bool =
 
-  if windowNode.currentColumn == 0: return false
-  let wordFirstLast = extractWordBeforeCursor(bufStatus, windowNode)
-  wordFirstLast.isSome and wordFirstLast.get.word.len > 0
+    if windowNode.currentColumn == 0: return false
+    let wordFirstLast = extractWordBeforeCursor(bufStatus, windowNode)
+    wordFirstLast.isSome and wordFirstLast.get.word.len > 0
 
 # Get a text in the buffer and language keywords
 proc getBufferAndLangKeyword(
@@ -175,14 +175,14 @@ proc getBufferAndLangKeyword(
   firstDeletedIndex, lastDeletedIndex: int,
   lang: SourceLanguage): seq[Rune] =
 
-  let
-    bufferText = getTextInBuffers(
-      checkBuffers,
-      firstDeletedIndex,
-      lastDeletedIndex)
-    keywordsText = getTextInLangKeywords(lang)
+    let
+      bufferText = getTextInBuffers(
+        checkBuffers,
+        firstDeletedIndex,
+        lastDeletedIndex)
+      keywordsText = getTextInLangKeywords(lang)
 
-  return bufferText & keywordsText
+    return bufferText & keywordsText
 
 proc buildSuggestionWindow*(
   wordDictionary: var WordDictionary,
@@ -190,78 +190,78 @@ proc buildSuggestionWindow*(
   currentBufferIndex: int,
   root, currenWindowNode: WindowNode): Option[SuggestionWindow] =
 
-  let
-    currentBufStatus = bufStatus[currentBufferIndex]
-    currentLineBuffer = currentBufStatus.buffer[currenWindowNode.currentLine]
-
-    # Whether the word on the current position is a path.
-    head = currentLineBuffer[0 .. currenWindowNode.currentColumn - 1]
-    word = (head.splitWhitespace)[^1].removePrefix("\"".ru)
-    isPath = word.isPath
-
-  if isPath:
-    let
-      (path, firstColumn, lastColumn) = extractPathBeforeCursor(
-        currentBufStatus,
-        currenWindowNode).get
-
-      (pathHead, pathTail) = splitPathExt(path)
-
-      text = getPathList(path)
-
-      # TODO: Fix and refactor
-      first =
-        if pathHead.high >= 0: firstColumn + pathHead.high
-        else: 0
-      last =
-        if pathTail.len == 0: first
-        else: lastColumn
-
-    initSuggestionWindow(
-      wordDictionary,
-      text,
-      pathTail,
-      currentBufStatus.buffer[currenWindowNode.currentLine],
-      first,
-      last,
-      isPath)
-
-  else:
     let
       currentBufStatus = bufStatus[currentBufferIndex]
-      (word, firstColumn, lastColumn) = extractWordBeforeCursor(
-        currentBufStatus,
-        currenWindowNode).get
+      currentLineBuffer = currentBufStatus.buffer[currenWindowNode.currentLine]
 
-      # Eliminate the word on the cursor.
-      line = currenWindowNode.currentLine
-      column = firstColumn
-      firstDeletedIndex = currentBufStatus.buffer.calcIndexInEntireBuffer(
-        line,
-        column,
-        true)
-      lastDeletedIndex = firstDeletedIndex + word.len - 1
-      bufferIndexList = root.getAllBufferIndex
+      # Whether the word on the current position is a path.
+      head = currentLineBuffer[0 .. currenWindowNode.currentColumn - 1]
+      word = (head.splitWhitespace)[^1].removePrefix("\"".ru)
+      isPath = word.isPath
 
-    # 0 is current bufStatus
-    var checkBuffers: seq[BufferStatus] = @[bufStatus[currentBufferIndex]]
-    for i in bufferIndexList:
-      if i != currentBufferIndex: checkBuffers.add bufStatus[i]
+    if isPath:
+      let
+        (path, firstColumn, lastColumn) = extractPathBeforeCursor(
+          currentBufStatus,
+          currenWindowNode).get
 
-    let text = getBufferAndLangKeyword(
-      checkBuffers,
-      firstDeletedIndex,
-      lastDeletedIndex,
-      bufStatus[currentBufferIndex].language)
+        (pathHead, pathTail) = splitPathExt(path)
 
-    initSuggestionWindow(
-      wordDictionary,
-      text,
-      word,
-      currentBufStatus.buffer[currenWindowNode.currentLine],
-      firstColumn,
-      lastColumn,
-      isPath)
+        text = getPathList(path)
+
+        # TODO: Fix and refactor
+        first =
+          if pathHead.high >= 0: firstColumn + pathHead.high
+          else: 0
+        last =
+          if pathTail.len == 0: first
+          else: lastColumn
+
+      initSuggestionWindow(
+        wordDictionary,
+        text,
+        pathTail,
+        currentBufStatus.buffer[currenWindowNode.currentLine],
+        first,
+        last,
+        isPath)
+
+    else:
+      let
+        currentBufStatus = bufStatus[currentBufferIndex]
+        (word, firstColumn, lastColumn) = extractWordBeforeCursor(
+          currentBufStatus,
+          currenWindowNode).get
+
+        # Eliminate the word on the cursor.
+        line = currenWindowNode.currentLine
+        column = firstColumn
+        firstDeletedIndex = currentBufStatus.buffer.calcIndexInEntireBuffer(
+          line,
+          column,
+          true)
+        lastDeletedIndex = firstDeletedIndex + word.len - 1
+        bufferIndexList = root.getAllBufferIndex
+
+      # 0 is current bufStatus
+      var checkBuffers: seq[BufferStatus] = @[bufStatus[currentBufferIndex]]
+      for i in bufferIndexList:
+        if i != currentBufferIndex: checkBuffers.add bufStatus[i]
+
+      let text = getBufferAndLangKeyword(
+        checkBuffers,
+        firstDeletedIndex,
+        lastDeletedIndex,
+        bufStatus[currentBufferIndex].language)
+
+      initSuggestionWindow(
+        wordDictionary,
+        text,
+        word,
+        currentBufStatus.buffer[currenWindowNode.currentLine],
+        firstColumn,
+        lastColumn,
+        isPath)
 
 proc tryOpenSuggestionWindow*(
   wordDictionary: var WordDictionary,
@@ -269,43 +269,43 @@ proc tryOpenSuggestionWindow*(
   currentBufferIndex: int,
   root, currenWindowNode: WindowNode): Option[SuggestionWindow] =
 
-  if wordExistsBeforeCursor(bufStatus[currentBufferIndex], currenWindowNode):
-    return buildSuggestionWindow(
-      wordDictionary,
-      bufStatus,
-      currentBufferIndex,
-      root,
-      currenWindowNode)
+    if wordExistsBeforeCursor(bufStatus[currentBufferIndex], currenWindowNode):
+      return buildSuggestionWindow(
+        wordDictionary,
+        bufStatus,
+        currentBufferIndex,
+        root,
+        currenWindowNode)
 
 proc calcSuggestionWindowPosition*(
   suggestionWindow: SuggestionWindow,
   windowNode: WindowNode,
   mainWindowHeight: int): tuple[y, x: int] =
 
-  let
-    line = windowNode.currentLine
-    column = suggestionWindow.firstColumn
-    (absoluteY, absoluteX) = windowNode.absolutePosition(line, column)
-    diffY = 1
-    leftMargin = 1
+    let
+      line = windowNode.currentLine
+      column = suggestionWindow.firstColumn
+      (absoluteY, absoluteX) = windowNode.absolutePosition(line, column)
+      diffY = 1
+      leftMargin = 1
 
-    # If the suggest window height is higher than the main window height under the cursor position,
-    # the suggest window  move to over the cursor position
-    suggestHigh = suggestionWindow.suggestoins.high
-    y =
-      if suggestHigh > (mainWindowHeight - absoluteY - diffY) and
-        absoluteY > (mainWindowHeight - absoluteY):
-        max(absoluteY - suggestHigh - diffY, 0)
-      else:
-        absoluteY + diffY
+      # If the suggest window height is higher than the main window height under the cursor position,
+      # the suggest window  move to over the cursor position
+      suggestHigh = suggestionWindow.suggestoins.high
+      y =
+        if suggestHigh > (mainWindowHeight - absoluteY - diffY) and
+          absoluteY > (mainWindowHeight - absoluteY):
+          max(absoluteY - suggestHigh - diffY, 0)
+        else:
+          absoluteY + diffY
 
-    x =
-      if suggestionWindow.isPath and suggestionWindow.oldLine.count('/'.ru) > 1:
-        absoluteX - leftMargin + 1
-      else:
-        absoluteX - leftMargin
+      x =
+        if suggestionWindow.isPath and suggestionWindow.oldLine.count('/'.ru) > 1:
+          absoluteX - leftMargin + 1
+        else:
+          absoluteX - leftMargin
 
-  return (y, x)
+    return (y, x)
 
 # cursorPosition is absolute y
 proc calcMaxSugestionWindowHeight(
@@ -314,13 +314,13 @@ proc calcMaxSugestionWindowHeight(
   mainWindowNodeY: int,
   isEnableStatusLine: bool): int =
 
-  const commanLineHeight = 1
-  let statusLineHeight = if isEnableStatusLine: 1 else: 0
+    const commanLineHeight = 1
+    let statusLineHeight = if isEnableStatusLine: 1 else: 0
 
-  if y > cursorYPosition:
-    result = (getTerminalHeight() - 1) - cursorYPosition - commanLineHeight - statusLineHeight
-  else:
-    result = cursorYPosition - mainWindowNodeY
+    if y > cursorYPosition:
+      result = (getTerminalHeight() - 1) - cursorYPosition - commanLineHeight - statusLineHeight
+    else:
+      result = cursorYPosition - mainWindowNodeY
 
 proc writeSuggestionWindow*(
   suggestionWindow: var SuggestionWindow,
@@ -329,43 +329,43 @@ proc writeSuggestionWindow*(
   mainWindowNodeY: int,
   isEnableStatusLine: bool) =
 
-  let
-    line = windowNode.currentLine
-    column = windowNode.currentColumn
-    (absoluteY, _) = windowNode.absolutePosition(line, column)
-    maxHeight = calcMaxSugestionWindowHeight(
-      y,
-      absoluteY,
-      mainWindowNodeY,
-      isEnableStatusLine)
-    height = min(suggestionWindow.suggestoins.len, maxHeight)
-    width = suggestionWindow.suggestoins.map(item => item.len).max + 2
+    let
+      line = windowNode.currentLine
+      column = windowNode.currentColumn
+      (absoluteY, _) = windowNode.absolutePosition(line, column)
+      maxHeight = calcMaxSugestionWindowHeight(
+        y,
+        absoluteY,
+        mainWindowNodeY,
+        isEnableStatusLine)
+      height = min(suggestionWindow.suggestoins.len, maxHeight)
+      width = suggestionWindow.suggestoins.map(item => item.len).max + 2
 
-  if suggestionWindow.popUpWindow.isNone:
-    suggestionWindow.popUpWindow = initWindow(
-      height,
-      width,
-      if y < mainWindowNodeY: mainWindowNodeY else: y,
-      x,
-      EditorColorPair.popUpWindow)
-      .some
-  else:
-    suggestionWindow.popUpWindow.get.height = height
-    suggestionWindow.popUpWindow.get.width = width
-    suggestionWindow.popUpWindow.get.y = y
-    suggestionWindow.popUpWindow.get.x = x
+    if suggestionWindow.popUpWindow.isNone:
+      suggestionWindow.popUpWindow = initWindow(
+        height,
+        width,
+        if y < mainWindowNodeY: mainWindowNodeY else: y,
+        x,
+        EditorColorPair.popUpWindow)
+        .some
+    else:
+      suggestionWindow.popUpWindow.get.height = height
+      suggestionWindow.popUpWindow.get.width = width
+      suggestionWindow.popUpWindow.get.y = y
+      suggestionWindow.popUpWindow.get.x = x
 
-  let currentLine =
-    if suggestionWindow.selectedSuggestion == -1: none(int)
-    else: suggestionWindow.selectedSuggestion.some
+    let currentLine =
+      if suggestionWindow.selectedSuggestion == -1: none(int)
+      else: suggestionWindow.selectedSuggestion.some
 
-  suggestionWindow.popUpWindow.get.writePopUpWindow(
-    suggestionWindow.popUpWindow.get.height,
-    suggestionWindow.popUpWindow.get.width,
-    suggestionWindow.popUpWindow.get.y,
-    suggestionWindow.popUpWindow.get.x,
-    currentLine,
-    suggestionWindow.suggestoins)
+    suggestionWindow.popUpWindow.get.writePopUpWindow(
+      suggestionWindow.popUpWindow.get.height,
+      suggestionWindow.popUpWindow.get.width,
+      suggestionWindow.popUpWindow.get.y,
+      suggestionWindow.popUpWindow.get.x,
+      currentLine,
+      suggestionWindow.suggestoins)
 
 proc isLineChanged*(suggestionWindow: SuggestionWindow): bool {.inline.} =
   suggestionWindow.newLine != suggestionWindow.oldLine
