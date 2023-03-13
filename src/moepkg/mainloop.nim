fox0430/moe

commit 106699359a1664070acd613910f961bd3fb6c85b

Add src/moepkg/mainloop.nim and large refactoring (#1571)
* Add src/moepkg/mainloop.nim and large refactoring

Editor main loops that were divided by mode are now unified into two loops, one for the main
window and one for the command line.

This isn't still perfect yet but will be merged because the changes are too
big.

This is probably buggy.

* test: Rename and fix tcommandlineutils.nim

* test: Fix tests/tdebugmode.nim

* Fix editorstatus.initBufferStatus and editorstatus.initSyntaxHighlight

* test: Fix tests/tfilermode.nim

* test: Fix tests/tinsertmode.nim

* Fix replacemode.replaceCurrentCharacter

* test: Fix tests/treplacemode.nim

* test: Fix tests/tsuggestwindow.nim

* test: Fix tests/tunicodeext.nim

* Fix visualmode.visualCommand, visualmode.visualBlockCommand

* test: Fix tests/tvisualmode.nim

* Rename: src/moepkg/filerutils.nim -> src/moepkg/filermodeutils.nim

* filer: Reset the current line after open a dir/file

* Fix filer mode bugs

Fix the ex mode prompt in the filer mode.

Fix the filer mode view update delay when opnning a dir/file

* Remove filerutils.filerStatus and add editorStatus.filerStatuses

* Fix unit tests

* Update doc

* Create fresh Scriv fragment

* Fix scriv fragment

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

src/moepkg/mainloop.nim
@@ -1,0 +1,341 @@
+import std/[terminal, options, times]
+
+import editorstatus, bufferstatus, window, unicodeext, gapbuffer, ui,
+       normalmode, visualmode, insertmode, autocomplete, suggestionwindow,
+       exmode, replacemode, filermode, buffermanager, logviewer, help,
+       recentfilemode, quickrun, backupmanager, diffviewer, configmode,
+       debugmode, commandline, search, commandlineutils, popupwindow,
+       filermodeutils
+
+proc searchCommand(currentMode: Mode, command: Runes): InputState =
+  case currentMode:
+    of Mode.insert, Mode.searchForward, Mode.searchBackward:
+      InputState.Valid
+    of Mode.ex:
+      isExCommand(command)
+    of Mode.normal:
+      isNormalModeCommand(command)
+    of Mode.visual, Mode.visualBlock:
+      isVisualModeCommand(command)
+    of Mode.replace:
+      isReplaceModeCommand(command)
+    of Mode.filer:
+      isFilerModeCommand(command)
+    of Mode.bufManager:
+      isBufferManagerCommand(command)
+    of Mode.logViewer:
+      isLogViewerCommand(command)
+    of Mode.help:
+      isHelpCommand(command)
+    of Mode.recentFile:
+      isRecentFileCommand(command)
+    of Mode.quickRun:
+      isQuickRunCommand(command)
+    of Mode.backup:
+      isBackupManagerCommand(command)
+    of Mode.diff:
+      isDiffViewerCommand(command)
+    of Mode.config:
+      isConfigModeCommand(command)
+    of Mode.debug:
+      isDebugModeCommand(command)
+
+proc execCommand(status: var EditorStatus, command: Runes) =
+  let currentMode = currentBufStatus.mode
+  case currentMode:
+    of Mode.insert:
+      status.execInsertModeCommand(command)
+    of Mode.ex:
+      status.execExCommand(command)
+    of Mode.normal:
+      status.execNormalModeCommand(command)
+    of Mode.visual, Mode.visualBlock:
+      status.execVisualModeCommand(command)
+    of Mode.replace:
+      status.execReplaceModeCommand(command)
+    of Mode.searchForward, Mode.searchBackward:
+      status.execSearchCommand(command)
+    of Mode.filer:
+      status.execFilerModeCommand(command)
+    of Mode.bufManager:
+      status.execBufferManagerCommand(command)
+    of Mode.logviewer:
+      status.execLogViewerCommand(command)
+    of Mode.help:
+      status.execHelpCommand(command)
+    of Mode.recentFile:
+      status.execRecentFileCommand(command)
+    of Mode.quickRun:
+      status.execQuickRunCommand(command)
+    of Mode.backup:
+      status.execBackupManagerCommand(command)
+    of Mode.diff:
+      status.execDiffViewerCommand(command)
+    of Mode.config:
+      status.execConfigCommand(command)
+    of Mode.debug:
+      status.execDebugModeCommand(command)
+
+proc isOpenSuggestWindow(status: EditorStatus): bool {.inline.} =
+  status.settings.autocomplete.enable and isInsertMode(currentBufStatus.mode)
+
+proc tryOpenSuggestWindow(status: var EditorStatus) {.inline.} =
+  status.suggestionWindow = tryOpenSuggestionWindow(
+    status.wordDictionary,
+    status.bufStatus,
+    currentMainWindowNode.bufferIndex,
+    mainWindowNode,
+    currentMainWindowNode)
+
+proc updateSuggestWindow(
+  status: var EditorStatus,
+  suggestWin: var SuggestionWindow) =
+
+    let
+      mainWindowY =
+        if status.settings.tabLine.enable: 1
+        else: 0
+      mainWindowHeight = status.settings.getMainWindowHeight(terminalHeight())
+      (y, x) = suggestWin.calcSuggestionWindowPosition(
+        currentMainWindowNode,
+        mainWindowHeight)
+
+    suggestWin.writeSuggestionWindow(
+      currentMainWindowNode,
+      y, x,
+      terminalHeight(), terminalWidth(),
+      mainWindowY,
+      status.settings.statusLine.enable)
+
+proc updateSelectedArea(status: var EditorStatus) {.inline.} =
+  currentBufStatus.selectArea.updateSelectedArea(
+    currentMainWindowNode.currentLine,
+    currentMainWindowNode.currentColumn)
+
+proc isIncrementalSearch(status: EditorStatus): bool {.inline.} =
+  isSearchMode(currentBufStatus.mode) and
+  status.settings.incrementalSearch
+
+proc decListIndex(index: var int, list: seq[Runes]) =
+  if index == 0: index = list.high
+  else: index .dec
+
+proc incListIndex(index: var int, list: seq[Runes]) =
+  if index == list.high: index = 0
+  else: index .inc
+
+## Get keys and update view.
+proc commandLineLoop*(status: var EditorStatus) =
+  # TODO: Remove
+  template openSuggestWindow() =
+    suggestType = status.commandLine.buffer.getSuggestType
+    suggestList = status.commandLine.getSuggestList(suggestType)
+    if suggestList.len > 0:
+      suggestWin = tryOpenSuggestWindow()
+
+  if currentBufStatus.isSearchMode:
+    status.searchHistory.add "".toRunes
+
+    if status.isIncrementalSearch:
+      status.isSearchHighlight = true
+
+  var
+    isCancel = false
+
+    # TODO: Change type to `SuggestionWindow`.
+    suggestWin = none(Window)
+
+    # Use only when Ex mode.
+    # TODO: Move
+    suggestType = SuggestType.exCommand
+
+    # Use when Ex mode and search mode.
+    # TODO: Move
+    suggestList: seq[Runes]
+
+    # Use when Ex mode and search mode.
+    # TODO: Move
+    suggestIndex =
+      if currentBufStatus.isSearchMode: status.searchHistory.high
+      else: 0
+
+  while not isCancel:
+    status.update
+
+    if suggestWin.isSome:
+      suggestWin.get.updateSuggestWindow(
+        suggestType,
+        suggestList,
+        suggestIndex,
+        status.commandLine)
+      status.commandLine.update
+
+    # TODO: Move to editorstatus.update?
+    if currentBufStatus.isSearchMode:
+      status.commandLine.buffer = status.searchHistory[suggestIndex]
+      status.commandLine.update
+
+    let key = status.getKeyFromCommandLine
+
+    if isResizekey(key):
+      status.resize
+    elif isEscKey(key) or isControlC(key):
+      isCancel = true
+      if suggestWin.isSome:
+        suggestWin.get.delete
+        suggestWin = none(Window)
+    elif isEnterKey(key):
+      if suggestWin.isSome:
+        suggestWin.get.delete
+        suggestWin = none(Window)
+      break
+
+    elif isTabKey(key):
+      if suggestWin.isNone: openSuggestWindow()
+      else: suggestIndex.incListIndex(suggestList)
+
+      continue
+    elif isShiftTab(key):
+      if suggestWin.isNone: openSuggestWindow()
+      else: suggestIndex.decListIndex(suggestList)
+
+      continue
+    elif isLeftKey(key):
+      status.commandLine.moveLeft
+    elif isRightkey(key):
+      status.commandLine.moveRight
+      if status.settings.popUpWindowInExmode:
+        if status.popupWIndow != nil:
+          status.popupWIndow.deleteWindow
+          continue
+    elif isUpKey(key):
+      if currentBufStatus.isExMode:
+        suggestIndex.decListIndex(suggestList)
+      elif currentBufStatus.isSearchMode:
+        suggestIndex.decListIndex(status.searchHistory)
+      continue
+    elif isDownKey(key):
+      if currentBufStatus.isExMode:
+        suggestIndex.incListIndex(suggestList)
+      elif currentBufStatus.isSearchMode:
+        suggestIndex.incListIndex(status.searchHistory)
+      continue
+    elif isHomeKey(key):
+      status.commandLine.moveTop
+    elif isEndKey(key):
+      status.commandLine.moveEnd
+    elif isBackspaceKey(key):
+      status.commandLine.deleteChar
+    elif isDcKey(key):
+      status.commandLine.deleteCurrentChar
+    else:
+      status.commandLine.insert(key)
+
+      if status.isIncrementalSearch:
+        status.execSearchCommand(status.commandLine.buffer)
+
+    if suggestWin.isSome:
+      suggestWin.get.delete
+      suggestWin = none(Window)
+
+  if isCancel:
+    status.changeMode(currentBufStatus.prevMode)
+
+    if currentBufStatus.isSearchMode:
+      if status.searchHistory[^1].len == 0:
+        status.searchHistory.delete(status.searchHistory.high)
+
+      if status.isIncrementalSearch:
+        status.isSearchHighlight = false
+  else:
+    if isExMode(currentBufStatus.mode):
+      let command = status.commandLine.buffer
+
+      if searchCommand(currentBufStatus.mode, command) == InputState.Valid:
+        status.execCommand(command)
+
+      if isExMode(currentBufStatus.mode):
+        status.changeMode(currentBufStatus.prevMode)
+    elif isSearchMode(currentBufStatus.mode):
+      status.changeMode(currentBufStatus.prevMode)
+
+proc updateAfterInsertFromSuggestion(status: var EditorStatus) =
+  if status.suggestionWindow.get.isLineChanged:
+    currentBufStatus.buffer[currentMainWindowNode.currentLine] =
+      status.suggestionWindow.get.newLine
+    currentMainWindowNode.expandedColumn = currentMainWindowNode.currentColumn
+
+  # Update WordDictionary
+  block:
+    let selectedWord = status.suggestionWindow.get.getSelectedWord
+    if selectedWord.len > 0:
+      status.wordDictionary.incNumOfUsed(selectedWord)
+
+# TODO: Move
+proc close(suggestWin: var Option[SuggestionWindow]) {.inline.} =
+  suggestWin.get.close
+  suggestWin = none(SuggestionWindow)
+
+## Get keys and update view.
+proc editorMainLoop*(status: var EditorStatus) =
+  status.resize
+
+  var command: Runes
+
+  while mainWindow.numOfMainWindow > 0:
+    if currentBufStatus.isEditMode:
+      # For undo/redo
+      currentBufStatus.buffer.beginNewSuitIfNeeded
+      currentBufStatus.tryRecordCurrentPosition(currentMainWindowNode)
+
+    # TODO: Move to editorstatus.update
+    if isVisualMode(currentBufStatus.mode):
+      status.updateSelectedArea
+
+    status.update
+
+    # TODO: Move to editorStatus.update?
+    if status.suggestionWindow.isSome:
+      status.updateSuggestWindow(status.suggestionWindow.get)
+
+    let key = status.getKeyFromMainWindow
+
+    if status.suggestionWindow.isSome:
+      if canHandleInSuggestionWindow(key):
+        status.suggestionWindow.get.handleKeyInSuggestionWindow(
+          currentBufStatus,
+          currentMainWindowNode,
+          key)
+        continue
+      else:
+        status.updateAfterInsertFromSuggestion
+        status.suggestionWindow.close
+
+    if isResizekey(key):
+      status.resize
+      continue
+
+    command.add key
+
+    let inputState = searchCommand(currentBufStatus.mode, command)
+    case inputState:
+      of Continue:
+        continue
+      of Valid:
+        status.lastOperatingTime = now()
+        status.execCommand(command)
+        command.clear
+      of Invalid, Cancel:
+        command.clear
+        currentBufStatus.cmdLoop = 0
+        continue
+
+    if status.isOpenSuggestWindow:
+      status.tryOpenSuggestWindow
+
+    # TODO: Fix condition.
+    # I think this should use something like a flag or enum
+    # for switching to the command line instead of modes.
+    if currentBufStatus.isExmode or
+       currentBufStatus.isSearchMode:
+         status.commandLineLoop

src/moe.nim
@@ -1,8 +1,6 @@
 import std/[os, times]
-import moepkg/[ui, editorstatus, normalmode, insertmode, visualmode,
-               replacemode, filermode, exmode, buffermanager, logviewer,
-               cmdlineoption, bufferstatus, help, recentfilemode, quickrun,
-               backupmanager, diffviewer, configmode, debugmode]
+
+import moepkg/[ui, bufferstatus, editorstatus, cmdlineoption, mainloop]
 
 # Load persisted data (Ex command history, search history and cursor postion)
 proc loadPersistData(status: var EditorStatus) =
@@ -17,17 +15,15 @@ proc loadPersistData(status: var EditorStatus) =
     currentMainWindowNode.restoreCursorPostion(currentBufStatus,
                                                status.lastPosition)
 
-proc addBufferStatus(status: var EditorStatus,
-                     parsedList: CmdParsedList) =
-
+proc addBufferStatus(status: var EditorStatus, parsedList: CmdParsedList) =
   if parsedList.path.len > 0:
     for path in parsedList.path:
       if dirExists(path):
-        status.addNewBuffer(path, Mode.filer)
+        status.addNewBufferInCurrentWin(path, Mode.filer)
       else:
-        status.addNewBuffer(path)
+        status.addNewBufferInCurrentWin(path)
   else:
-    status.addNewBuffer
+    status.addNewBufferInCurrentWin
 
 proc initEditor(): EditorStatus =
   let parsedList = parseCommandLineOption(commandLineParams())
@@ -55,25 +51,9 @@ proc initEditor(): EditorStatus =
 proc main() =
   var status = initEditor()
 
-  while status.mainWindow.numOfMainWindow > 0:
-
-    case currentBufStatus.mode:
-    of Mode.normal: status.normalMode
-    of Mode.insert: status.insertMode
-    of Mode.visual, Mode.visualBlock: status.visualMode
-    of Mode.replace: status.replaceMode
-    of Mode.ex: status.exMode
-    of Mode.filer: status.filerMode
-    of Mode.bufManager: status.bufferManager
-    of Mode.logViewer: status.messageLogViewer
-    of Mode.help: status.helpMode
-    of Mode.recentFile: status.recentFileMode
-    of Mode.quickRun: status.quickRunMode
-    of Mode.backup: status.backupManager
-    of Mode.diff: status.diffViewer
-    of Mode.config: status.configMode
-    of Mode.debug: status.debugMode
+  status.editorMainLoop
 
   status.exitEditor
 
 when isMainModule: main()
+

src/moepkg/autocomplete.nim
@@ -1,6 +1,6 @@
 import std/[sugar, critbits, options, sequtils, strutils]
 import pkg/unicodedb/properties
-import unicodeext, bufferstatus, algorithm, osext
+import unicodeext, bufferstatus, algorithm, osext, gapbuffer
 import syntax/[ highlite
               , syntaxc
               , syntaxcpp

src/moepkg/backupmanager.nim
@@ -3,7 +3,7 @@
 import std/[os, times, terminal, osproc, json, strformat]
 import editorstatus, bufferstatus, unicodeext, ui, movement, gapbuffer,
        highlight, color, settings, messages, backup, fileutils, editorview,
-       window, commandviewutils
+       window, commandlineutils
 
 proc initBackupManagerBuffer(
   bufStatus: var BufferStatus,
@@ -73,7 +73,7 @@ proc openDiffViewer(status: var Editorstatus, sourceFilePath: string) =
   status.resize(terminalHeight(), terminalWidth())
   status.moveNextWindow
 
-  status.addNewBuffer(Mode.diff)
+  status.addNewBufferInCurrentWin(Mode.diff)
   status.changeCurrentBuffer(status.bufStatus.high)
 
   currentBufStatus.path = backupFilePath.toRunes
@@ -122,7 +122,7 @@ proc restoreBackupFile(
       if status.bufStatus[i].absolutePath == sourceFilePath:
         let beforeBufStatus = status.bufStatus[i]
 
-        status.bufStatus[i] = initBufferStatus(sourceFilePath)
+        status.bufStatus[i] = initBufferStatus($sourceFilePath)
 
         try:
           let textAndEncoding = openFile(sourceFilePath)
@@ -193,6 +193,80 @@ template removeBackupFile(status: var EditorStatus, sourceFilePath: seq[Rune]) =
   const IS_FORCE_REMOVE = false
   status.removeBackupFile(sourceFilePath, IS_FORCE_REMOVE)
 
+proc isBackupManagerCommand*(command: Runes): InputState =
+  result = InputState.Invalid
+
+  if command.len == 1:
+    let key = command[0]
+    if isControlK(key) or
+       isControlJ(key) or
+       key == ord(':') or
+       key == ord('k') or isUpKey(key) or
+       key == ord('j') or isDownKey(key) or
+       isEnterKey(key) or
+       key == ord('R') or
+       key == ord('D') or
+       key == ord('r') or
+       key == ord('G'):
+         return InputState.Valid
+    elif key == ord('g'):
+      return InputState.Continue
+  elif command.len == 2:
+    if command[0] == ord('g'):
+      if command[1] == ord('g'):
+        return InputState.Valid
+
+proc execBackupManagerCommand*(status: var EditorStatus, command: Runes) =
+  let sourceFilePath = status.bufStatus[status.prevBufferIndex].absolutePath
+
+  # TODO: Move
+  block:
+    currentBufStatus.initBackupManagerBuffer(
+      status.baseBackupDir,
+      sourceFilePath)
+
+    let
+      currentLine = currentMainWindowNode.currentLine
+      highlight = currentBufStatus.initBackupManagerHighlight(currentLine)
+    currentMainWindowNode.highlight = highlight
+
+    status.update
+    setCursor(false)
+
+  if command.len == 1:
+    let key = command[0]
+    if isControlK(key):
+      status.moveNextWindow
+    elif isControlJ(key):
+      status.movePrevWindow
+    elif key == ord(':'):
+      status.changeMode(Mode.ex)
+    elif key == ord('k') or isUpKey(key):
+      currentBufStatus.keyUp(currentMainWindowNode)
+    elif key == ord('j') or isDownKey(key):
+      currentBufStatus.keyDown(currentMainWindowNode)
+    elif isEnterKey(key):
+      status.openDiffViewer($sourceFilePath)
+    elif key == ord('R'):
+      status.restoreBackupFile(sourceFilePath)
+    elif key == ord('D'):
+      status.removeBackupFile(sourceFilePath)
+      currentBufStatus.initBackupManagerBuffer(
+        status.baseBackupDir,
+        sourceFilePath)
+    elif key == ord('r'):
+      # Reload backup files
+      currentBufStatus.initBackupManagerBuffer(
+        status.baseBackupDir,
+        sourceFilePath)
+    elif key == ord('G'):
+      currentBufStatus.moveToLastLine(currentMainWindowNode)
+  elif command.len == 2:
+    if command[0] == ord('g'):
+      if command[1] == ord('g'):
+        currentBufStatus.moveToFirstLine(currentMainWindowNode)
+
+# TODO: Remove
 proc backupManager*(status: var EditorStatus) =
   status.resize(terminalHeight(), terminalWidth())
 
@@ -212,8 +286,8 @@ proc backupManager*(status: var EditorStatus) =
     status.update
     setCursor(false)
 
-    var key = errorKey
-    while key == errorKey:
+    var key = ERR_KEY
+    while key == ERR_KEY:
       status.eventLoopTask
       key = getKey(currentMainWindowNode)
 

src/moepkg/buffermanager.nim
@@ -1,46 +1,20 @@
-import std/[terminal, os, heapqueue, times]
-import gapbuffer, ui, editorstatus, unicodeext, highlight, window, movement,
-       color, bufferstatus
+import std/[terminal, os, heapqueue]
+import gapbuffer, ui, editorstatus, unicodeext, window, movement, bufferstatus
 
-proc initFilelistHighlight[T](buffer: T,
-                              currentLine: int): Highlight =
+proc initBufferManagerBuffer*(
+  bufStatuses: seq[BufferStatus]): seq[Runes] =
+    for bufstatus in bufStatuses:
+      let currentMode = bufStatus.mode
+      if currentMode != Mode.bufManager:
+        let
+          prevMode = bufStatus.prevMode
+          line =
+            if (currentMode == Mode.filer) or
+               (prevMode == Mode.filer and
+                currentMode == Mode.ex): getCurrentDir().toRunes
+            else: bufStatus.path
 
-  for i in 0 ..< buffer.len:
-    let color =
-      if i == currentLine: EditorColorPair.currentLineNum
-      else: EditorColorPair.defaultChar
-
-    let colorSegment = ColorSegment(
-      firstRow: i,
-      firstColumn: 0,
-      lastRow: i,
-      lastColumn: buffer[i].len,
-      color: color)
-
-    result.colorSegments.add(colorSegment)
-
-proc setBufferList(status: var Editorstatus) =
-  currentBufStatus.path = ru"Buffer manager"
-  currentBufStatus.buffer = initGapBuffer[seq[Rune]]()
-
-  for i in 0 ..< status.bufStatus.len:
-    let currentMode = status.bufStatus[i].mode
-    if currentMode != Mode.bufManager:
-      let
-        prevMode = status.bufStatus[i].prevMode
-        line =
-          if (currentMode == Mode.filer) or
-            (prevMode == Mode.filer and
-            currentMode == Mode.ex): getCurrentDir().toRunes
-          else: status.bufStatus[i].path
-
-      currentBufStatus.buffer.add(line)
-
-proc updateBufferManagerHighlight[T](node: var WindowNode,
-                                     buffer: T,
-                                     currentLine: int) {.inline.} =
-
-  node.highlight = initFilelistHighlight(buffer, currentLine)
+        result.add line
 
 proc deleteSelectedBuffer(status: var Editorstatus, height, width: int) =
   let deleteIndex = currentMainWindowNode.currentLine
@@ -80,7 +54,8 @@ proc deleteSelectedBuffer(status: var Editorstatus, height, width: int) =
 
     let index = status.mainWindow.numOfMainWindow - 1
     currentMainWindowNode = mainWindowNode.searchByWindowIndex(index)
-    status.setBufferList
+    currentBufStatus.buffer =
+      status.bufStatus.initBufferManagerBuffer.toGapBuffer
 
     status.resize(terminalHeight(), terminalWidth())
 
@@ -93,51 +68,39 @@ proc openSelectedBuffer(status: var Editorstatus, isNewWindow: bool) =
     status.changeCurrentBuffer(currentMainWindowNode.currentLine)
     status.bufStatus.delete(status.bufStatus.high)
 
-proc bufferManager*(status: var Editorstatus) =
-  status.setBufferList
-  status.resize(terminalHeight(), terminalWidth())
+proc isBufferManagerCommand*(command: Runes): InputState =
+  result = InputState.Invalid
 
-  let currentBufferIndex = status.bufferIndexInCurrentWindow
+  if command.len == 1:
+    let key = command[0]
+    if isControlK(key) or
+       isControlJ(key) or
+       key == ord(':') or
+       key == ord('k') or isUpKey(key) or
+       key == ord('j') or isDownKey(key) or
+       isEnterKey(key) or
+       key == ord('o') or
+       key == ord('D'):
+         return InputState.Valid
 
-  while isBufferManagerMode(currentBufStatus.mode) and
-        currentBufferIndex == status.bufferIndexInCurrentWindow:
+proc execBufferManagerCommand*(status: var Editorstatus, command: Runes) =
+  let key = command[0]
 
-    block:
-      let
-        buffer = currentBufStatus.buffer
-        currentLine = currentMainWindowNode.currentLine
-      currentMainWindowNode.updateBufferManagerHighlight(buffer, currentLine)
+  if isControlK(key):
+    status.moveNextWindow
+  elif isControlJ(key):
+    status.movePrevWindow
+  elif key == ord(':'):
+    status.changeMode(Mode.ex)
+  elif key == ord('k') or isUpKey(key):
+    currentBufStatus.keyUp(currentMainWindowNode)
+  elif key == ord('j') or isDownKey(key):
+    currentBufStatus.keyDown(currentMainWindowNode)
+  elif isEnterKey(key):
+    status.openSelectedBuffer(false)
+  elif key == ord('o'):
+    status.openSelectedBuffer(true)
+  elif key == ord('D'):
+    status.deleteSelectedBuffer(terminalHeight(), terminalWidth())
 
-    status.update
-    setCursor(false)
-
-    var key = errorKey
-    while key == errorKey:
-      status.eventLoopTask
-      key = getKey(currentMainWindowNode)
-
-    status.lastOperatingTime = now()
-
-    if isResizekey(key):
-      status.resize(terminalHeight(), terminalWidth())
-
-    elif isControlK(key):
-      status.moveNextWindow
-    elif isControlJ(key):
-      status.movePrevWindow
-
-    elif key == ord(':'):
-      status.changeMode(Mode.ex)
-
-    elif key == ord('k') or isUpKey(key):
-      currentBufStatus.keyUp(currentMainWindowNode)
-    elif key == ord('j') or isDownKey(key):
-      currentBufStatus.keyDown(currentMainWindowNode)
-    elif isEnterKey(key):
-      status.openSelectedBuffer(false)
-    elif key == ord('o'):
-      status.openSelectedBuffer(true)
-    elif key == ord('D'):
-      status.deleteSelectedBuffer(terminalHeight(), terminalWidth())
-
-    if status.bufStatus.len < 2: status.exitEditor
+  if status.bufStatus.len < 2: status.exitEditor

commit a4b3691c4f930861cfea6cc802c438ca844858ae

Fix #1662
Fix a bug that cannot call ex command history and search history.
Caused by a change in #1571.

src/moepkg/mainloop.nim
@@ -133,6 +133,74 @@ proc incListIndex(index: var int, list: seq[Runes]) =
   if index == list.high: index = 0
   else: index .inc
 
+proc assignNextExCommandHistory(
+  status: var EditorStatus,
+  exCommandHistoryIndex: var Option[int]) =
+
+    let exCommandHistory = status.exCommandHistory
+
+    if exCommandHistory.len > 0 and exCommandHistoryIndex.isSome:
+      if exCommandHistoryIndex.get < exCommandHistory.high:
+        exCommandHistoryIndex.get.inc
+
+      status.commandLine.buffer = exCommandHistory[exCommandHistoryIndex.get]
+      status.commandLine.moveEnd
+
+proc assignPrevExCommandHistory(
+  status: var EditorStatus,
+  exCommandHistoryIndex: var Option[int]) =
+
+    let exCommandHistory = status.exCommandHistory
+
+    if exCommandHistory.len > 0:
+      if exCommandHistoryIndex.isNone:
+        exCommandHistoryIndex = exCommandHistory.high.some
+      elif exCommandHistoryIndex.get > 0:
+        exCommandHistoryIndex.get.dec
+
+      status.commandLine.buffer = exCommandHistory[exCommandHistoryIndex.get]
+      status.commandLine.moveEnd
+
+proc assignNextSearchHistory(
+  status: var EditorStatus,
+  searchHistoryIndex: var Option[int]) =
+
+    let searchHistory = status.searchHistory
+
+    if status.searchHistory.len > 0 and searchHistoryIndex.isSome:
+      if searchHistoryIndex.get < searchHistory.high:
+        searchHistoryIndex.get.inc
+
+      status.commandLine.buffer = searchHistory[searchHistoryIndex.get]
+      status.commandLine.moveEnd
+
+proc assignPrevSearchHistory(
+  status: var EditorStatus,
+  searchHistoryIndex: var Option[int]) =
+
+    let searchHistory = status.searchHistory
+
+    if status.searchHistory.len > 0:
+      if searchHistoryIndex.isNone:
+        searchHistoryIndex = searchHistory.high.some
+      elif searchHistoryIndex.get > 0:
+        searchHistoryIndex.get.dec
+
+      status.commandLine.buffer = searchHistory[searchHistoryIndex.get]
+      status.commandLine.moveEnd
+
+proc isResetExCommandHistoryIndex(
+  exCommandHistoryIndex: Option[int],
+  key: Rune): bool {.inline.} =
+
+    (exCommandHistoryIndex.isSome) and not (isUpKey(key) or isDownKey(key))
+
+proc isResetSearchHistoryIndex(
+  searchHistoryIndex: Option[int],
+  key: Rune): bool {.inline.} =
+
+    (searchHistoryIndex.isSome) and not (isUpKey(key) or isDownKey(key))
+
 ## Get keys and update view.
 proc commandLineLoop*(status: var EditorStatus) =
   # TODO: Remove
@@ -171,6 +239,12 @@ proc commandLineLoop*(status: var EditorStatus) =
       if currentBufStatus.isSearchMode: status.searchHistory.high
       else: 0
 
+    # TODO: Remove
+    exCommandHistoryIndex: Option[int]
+
+    # TODO: Remove
+    searchHistoryIndex: Option[int]
+
   while not isCancel:
     status.update
 
@@ -192,7 +266,15 @@ proc commandLineLoop*(status: var EditorStatus) =
     if isResizeKey(key):
       updateTerminalSize()
       status.resize
-    elif isEscKey(key) or isControlC(key):
+      continue
+
+    if exCommandHistoryIndex.isResetExCommandHistoryIndex(key):
+      exCommandHistoryIndex = none(int)
+
+    if searchHistoryIndex.isResetSearchHistoryIndex(key):
+      searchHistoryIndex = none(int)
+
+    if isEscKey(key) or isControlC(key):
       isCancel = true
       if suggestWin.isSome:
         suggestWin.get.delete
@@ -206,13 +288,12 @@ proc commandLineLoop*(status: var EditorStatus) =
     elif isTabKey(key):
       if suggestWin.isNone: openSuggestWindow()
       else: suggestIndex.incListIndex(suggestList)
-
       continue
     elif isShiftTab(key):
       if suggestWin.isNone: openSuggestWindow()
       else: suggestIndex.decListIndex(suggestList)
-
       continue
+
     elif isLeftKey(key):
       status.commandLine.moveLeft
     elif isRightKey(key):
@@ -221,18 +302,28 @@ proc commandLineLoop*(status: var EditorStatus) =
         if status.popupWindow != nil:
           status.popupWindow.deleteWindow
           continue
+
     elif isUpKey(key):
-      if currentBufStatus.isExMode:
+      if suggestWin.isSome and currentBufStatus.isExMode:
+        # The suggestion window is used only when suggesting ex commands.
         suggestIndex.decListIndex(suggestList)
-      elif currentBufStatus.isSearchMode:
-        suggestIndex.decListIndex(status.searchHistory)
-      continue
+      else:
+        if currentBufStatus.isSearchMode:
+          status.assignPrevSearchHistory(searchHistoryIndex)
+        else:
+          status.assignPrevExCommandHistory(exCommandHistoryIndex)
+          continue
     elif isDownKey(key):
-      if currentBufStatus.isExMode:
+      if suggestWin.isSome and currentBufStatus.isExMode:
+        # The suggestion window is used only when suggesting ex commands.
         suggestIndex.incListIndex(suggestList)
-      elif currentBufStatus.isSearchMode:
-        suggestIndex.incListIndex(status.searchHistory)
-      continue
+      else:
+        if currentBufStatus.isSearchMode:
+          status.assignNextSearchHistory(searchHistoryIndex)
+        else:
+          status.assignNextExCommandHistory(exCommandHistoryIndex)
+          continue
+
     elif isHomeKey(key):
       status.commandLine.moveTop
     elif isEndKey(key):
@@ -244,8 +335,8 @@ proc commandLineLoop*(status: var EditorStatus) =
     else:
       status.commandLine.insert(key)
 
-      if status.isIncrementalSearch:
-        status.execSearchCommand(status.commandLine.buffer)
+    if status.isIncrementalSearch and status.commandLine.buffer.len > 0:
+      status.execSearchCommand(status.commandLine.buffer)
 
     if suggestWin.isSome:
       suggestWin.get.delete