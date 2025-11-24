1d98f2fe3f53227976229fa55a4dc4dfb685c084
fox0430 <shuu.n@protonmail.com>
2025-11-24 21:50:03 +0900

    nph

--- tests/teditorstatus.nim
+++ tests/teditorstatus.nim
@@ -28,7 +28,7 @@ import
   moepkg/[
     editor, gapbuffer, bufferstatus, editorview, unicodeext, build, highlight,
     windownode, movement, backgroundprocess, syntaxcheck, independentutils, tabline,
-    settings, visualmode, statusline, buffercache
+    settings, visualmode, statusline, buffercache,
   ]
 
 import utils
