5dd7ecee30d23a3d0b99bef3268cda705ffcf457
fox0430 <shuu.n@protonmail.com>
2026-02-20 07:13:25 +0900

    Better test tool

--- tools/paralleltest.nim
+++ tools/paralleltest.nim
@@ -23,11 +23,29 @@ proc main() =
 
   let commands = testFiles.mapIt("nim c -r tests/" & it)
 
-  let exitCode =
-    execProcesses(commands, n = jobs, options = {poParentStreams, poUsePath})
+  var failedTests: seq[tuple[name: string, exitCode: int]]
 
-  echo "\nFinished " & $testFiles.len & " files"
-  echo "Exit code: ", exitCode
+  proc afterTest(idx: int, p: Process) =
+    let code = p.peekExitCode()
+    if code != 0:
+      failedTests.add((testFiles[idx], code))
+
+  let exitCode = execProcesses(
+    commands,
+    n = jobs,
+    options = {poParentStreams, poUsePath},
+    afterRunEvent = afterTest,
+  )
+
+  echo "\nFinished ", testFiles.len, " files"
+
+  if failedTests.len > 0:
+    echo "\nFailed (", failedTests.len, "/", testFiles.len, "):"
+    for (name, code) in failedTests:
+      echo "  FAIL: ", name, " (exit code: ", code, ")"
+    echo '\n'
+  else:
+    echo "\nAll tests passed."
 
   quit(exitCode)
 
