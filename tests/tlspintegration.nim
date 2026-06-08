6e8691ecaa26304e26cae816ad805b033866e793
fox0430 <shuu.n@protonmail.com>
2026-06-08 22:46:45 +0900

    nph

--- tests/tlspintegration.nim
+++ tests/tlspintegration.nim
@@ -1205,8 +1205,7 @@ suite "WorkspaceEdit Application":
           "edits": [
             {
               "range": {
-                "start": {"line": 0, "character": 0},
-                "end": {"line": 0, "character": 0},
+                "start": {"line": 0, "character": 0}, "end": {"line": 0, "character": 0}
               },
               "newText": "x",
             }
