b3589d60ca1248d1ab75ea4335fa9269566ceee3
Jaremy Creechley <creechley@gmail.com>
2026-07-27 04:15:38 +0300

    tweak

--- tests/test_editor_config_pull_accessors.nim
+++ tests/test_editor_config_pull_accessors.nim
@@ -285,7 +285,7 @@ suite "Editor - applyConfigSettings live-reload (S1 regression)":
 
     let request = e.state.takeMouseCaptureRequest()
     check request.isSome
-    check request.get == true
+    check request.get() == true
     check e.state.takeMouseCaptureRequest().isNone
 
 suite "Editor - applyConfigSettings LSP server configs":
