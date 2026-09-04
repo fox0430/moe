68946675b8834495f35c5e581b6f23b8031b2631
Jaremy Creechley <creechley@gmail.com>
2026-09-05 01:38:42 +0300

    Remove unused embedded clipboard commands

--- src/moepkg/clipboard_embedded.nim
+++ src/moepkg/clipboard_embedded.nim
@@ -23,80 +23,17 @@
 ## integration disabled. These routines remain available so editor modules have
 ## one stable interface, but report a clear error if called.
 
-import std/options
-
 import pkg/results
 
 import config
 
 type
-  ClipboardOperation* {.pure.} = enum
-    read
-    write
-
   ClipboardError* = object of CatchableError
 
 const
   WriteTimeoutMs = 10_000
   EmbeddedClipboardError =
     "System clipboard access is owned by the embedding frontend (-d:moe.embedded)"
-
-proc getClipboardCommand*(
-    tool: ClipboardTool, operation: ClipboardOperation
-): Option[seq[string]] =
-  case tool
-  of cbtXclip:
-    case operation
-    of ClipboardOperation.read:
-      some(@["xclip", "-selection", "clipboard", "-o"])
-    of ClipboardOperation.write:
-      some(@["xclip", "-selection", "clipboard", "-i"])
-  of cbtXsel:
-    case operation
-    of ClipboardOperation.read:
-      some(@["xsel", "--clipboard", "--output"])
-    of ClipboardOperation.write:
-      some(@["xsel", "--clipboard", "--input"])
-  of cbtWlClipboard:
-    case operation
-    of ClipboardOperation.read:
-      some(@["wl-paste", "-n"])
-    of ClipboardOperation.write:
-      some(@["wl-copy"])
-  of cbtWin32yank:
-    case operation
-    of ClipboardOperation.read:
-      some(@["win32yank.exe", "-o", "--lf"])
-    of ClipboardOperation.write:
-      some(@["win32yank.exe", "-i", "--crlf"])
-  of cbtPbcopy:
-    case operation
-    of ClipboardOperation.read:
-      some(@["pbpaste"])
-    of ClipboardOperation.write:
-      some(@["pbcopy"])
-
-proc getPrimarySelectionReadCommand*(tool: ClipboardTool): Option[seq[string]] =
-  case tool
-  of cbtXclip:
-    some(@["xclip", "-selection", "primary", "-o"])
-  of cbtXsel:
-    some(@["xsel", "--output"])
-  of cbtWlClipboard:
-    some(@["wl-paste", "-n", "--primary"])
-  of cbtWin32yank, cbtPbcopy:
-    getClipboardCommand(tool, ClipboardOperation.read)
-
-proc getPrimarySelectionWriteCommand*(tool: ClipboardTool): Option[seq[string]] =
-  case tool
-  of cbtXclip:
-    some(@["xclip", "-selection", "primary", "-i"])
-  of cbtXsel:
-    some(@["xsel", "--input"])
-  of cbtWlClipboard:
-    some(@["wl-copy", "--primary"])
-  of cbtWin32yank, cbtPbcopy:
-    getClipboardCommand(tool, ClipboardOperation.write)
 
 proc readFromClipboardSync*(tool: ClipboardTool): Result[string, string] =
   discard tool
